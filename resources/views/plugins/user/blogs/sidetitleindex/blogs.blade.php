b6c59ca402f9451e4fd56a5e047b355bb1249b41
horitada <m.horiguchi.info@gmail.com>
2019-10-24 13:25:00 +0900

    クラス名定義箇所を変更

--- resources/views/plugins/user/blogs/sidetitleindex/blogs.blade.php
+++ resources/views/plugins/user/blogs/sidetitleindex/blogs.blade.php
@@ -9,14 +9,16 @@
 
 {{-- ブログ表示 --}}
 @if (isset($blogs_posts))
+    <div class="sidetittleindex">
     @foreach($blogs_posts as $post)
-        <div class="sidetittleindexrow">
+        <div class="row">
         {{-- 投稿日時 --}}
         <span class="date">{{$post->posted_at->format('Y年n月j日')}}</span>
         {{-- タイトル --}}
         <a href="{{url('/')}}/plugin/blogs/show/{{$page->id}}/{{$frame_id}}/{{$post->id}}"><span class="tittle">{{$post->post_title}}</span></a>
         </div>
     @endforeach
+    </div>
 
     {{-- ページング処理 --}}
     {{--
