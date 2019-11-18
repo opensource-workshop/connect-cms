8dae81c602ed2dccbfe7e85a16a7015cb522b753
horitada <m.horiguchi.info@gmail.com>
2019-11-18 12:31:28 +0900

    class名の修正

--- database/migrations/2019_11_07_101849_create_categories_table.php
+++ database/migrations/2019_11_07_101849_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoriesTable2 extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
