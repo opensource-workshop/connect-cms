5a6950174a5bada48dccc920f1b6c103a0e212c4
horitada <m.horiguchi.info@gmail.com>
2019-11-28 16:55:13 +0900

    回層表示バグ修正

--- resources/views/plugins/user/menus/parentsandchild/menu_children.blade.php
+++ resources/views/plugins/user/menus/parentsandchild/menu_children.blade.php
@@ -1,28 +1,30 @@
 {{--
  * メニューの子要素表示画面
  *
- * @param obj $pages ページデータの配列
- * @author 永原　篤 <nagahara@opensource-workshop.jp>
+ * @param obj $children ページデータの配列
+ * @author 堀口 <horiguchi@opensource-workshop.jp>
  * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
  * @category メニュープラグイン
 --}}
+    @foreach($children as $page_obj)
+        @if ($page_obj->display_flag == 1)
 
-@if ($children->display_flag == 1)
+            @if ($page_obj->id == $page_id)
+            <a href="{{ url("$page_obj->permanent_link") }}" class="list-group-item active">
+            @else
+            <a href="{{ url("$page_obj->permanent_link") }}" class="list-group-item">
+            @endif
 
-        @if ($children->id == $page_id)
-        <a href="{{ url("$children->permanent_link") }}" class="list-group-item active">
-        @else
-        <a href="{{ url("$children->permanent_link") }}" class="list-group-item">
-        @endif
             {{-- 各ページの深さをもとにインデントの表現 --}}
-            @for ($i = 0; $i < $children->depth; $i++)
-                @if ($i+1==$children->depth) <i class="fas fa-chevron-right"></i> @else <span class="px-2"></span>@endif
+            @for ($i = 0; $i < $page_obj->depth; $i++)
+                @if ($i+1==$page_obj->depth) <i class="fas fa-chevron-right"></i> @else <span class="px-2"></span>@endif
             @endfor
-            {{$children->page_name}}
-        </a>
 
-        @if ($children->children && count($children->children) > 0)
-            @include('plugins.user.menus.parentsandchild.menu_children',['children' => $children->children[0]])
+            {{$page_obj->page_name}}</a>
+
+            @if (isset($page_obj->children))
+                @include('plugins.user.menus.parentsandchild2.menu_children',['children' => $page_obj->children])
+            @endif
+
         @endif
-
-@endif
+    @endforeach
