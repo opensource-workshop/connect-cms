b69f32b85b499606830bcce61d0caa8f23081ec6
masaton0216 <masamasamasato0216@gmail.com>
2019-12-17 11:46:08 +0900

    施設予約 リファクタリング コメント追加

--- resources/views/plugins/user/reservations/default/reservations_calendar_month.blade.php
+++ resources/views/plugins/user/reservations/default/reservations_calendar_month.blade.php
@@ -6,7 +6,8 @@
  * @category 施設予約プラグイン
  --}}
 
-<br>
+    {{-- カレンダーヘッダ部 --}}
+    <br>
     <div class="row">
         <div class="col-12 clearfix">
             <div class="float-left">
@@ -29,6 +30,8 @@
             </div>
         </div>
     </div>
+
+    {{-- カレンダーデータ部 --}}
     <table class="table table-bordered">
         <thead>
             {{-- カレンダーヘッダ部の曜日を表示 --}}
@@ -39,27 +42,27 @@
                 @endforeach
             </tr>
         </thead>
-    <tbody>
-        {{-- カレンダーデータ部の表示 --}}
-        @foreach ($dates as $date)
-            {{-- 日曜日なら新しい行 --}}
-            @if ($date->dayOfWeek == 0)
-                <tr>
-            @endif
-                    <td 
-                        {{-- 当月以外ならセル背景をグレーアウト --}}
-                        @if ($date->month != $carbon_target_date->month) class="bg-secondary" @endif
-                        {{-- 日曜なら赤文字 --}}
-                        @if ($date->dayOfWeek == 0) class="text-danger" @endif
-                        {{-- 日曜なら青文字 --}}
-                        @if ($date->dayOfWeek == 6) class="text-primary" @endif
-                    >
-                        {{ $date->day }}
-                    </td>
-            {{-- 土曜日なら行を閉じる --}}
-            @if ($date->dayOfWeek == 6)
-                </tr>
-            @endif
-        @endforeach
-    </tbody>
+        <tbody>
+            {{-- カレンダーデータ部の表示 --}}
+            @foreach ($dates as $date)
+                {{-- 日曜日なら新しい行 --}}
+                @if ($date->dayOfWeek == 0)
+                    <tr>
+                @endif
+                        <td 
+                            {{-- 当月以外ならセル背景をグレーアウト --}}
+                            @if ($date->month != $carbon_target_date->month) class="bg-secondary" @endif
+                            {{-- 日曜なら赤文字 --}}
+                            @if ($date->dayOfWeek == 0) class="text-danger" @endif
+                            {{-- 日曜なら青文字 --}}
+                            @if ($date->dayOfWeek == 6) class="text-primary" @endif
+                        >
+                            {{ $date->day }}
+                        </td>
+                {{-- 土曜日なら行を閉じる --}}
+                @if ($date->dayOfWeek == 6)
+                    </tr>
+                @endif
+            @endforeach
+        </tbody>
     </table>
