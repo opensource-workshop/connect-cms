3e3dbc40675da4fe8f4594a1bc4978b601e35805
masaton0216 <masamasamasato0216@gmail.com>
2019-12-21 12:38:36 +0900

    施設予約 登録できる予約項目の型を一旦絞る

--- app/Enums/ReservationColumnType.php
+++ app/Enums/ReservationColumnType.php
@@ -9,20 +9,20 @@ final class ReservationColumnType
 {
     // 定数メンバ
     const txt = 'text';
-    const txtarea = 'textarea';
+    // const txtarea = 'textarea';
     const radio = 'radio';
-    const checkbox = 'checkbox';
-    const select = 'select';
-    const datetime = 'datetime';
+    // const checkbox = 'checkbox';
+    // const select = 'select';
+    // const datetime = 'datetime';
 
     // key/valueの連想配列
     const enum = [
         self::txt=>'1行文字列型',
-        self::txtarea=>'複数行文字列型',
+        // self::txtarea=>'複数行文字列型',
         self::radio=>'単一選択型',
-        self::checkbox=>'複数選択型',
-        self::select=>'リストボックス型',
-        self::datetime=>'日付＆時間型',
+        // self::checkbox=>'複数選択型',
+        // self::select=>'リストボックス型',
+        // self::datetime=>'日付＆時間型',
     ];
 
     /*
