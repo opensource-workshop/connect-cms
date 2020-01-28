38dd357227c10b201f271adb9fef550306c426bb
オープンソース・ワークショップ 永原 篤 <nagahara@opensource-workshop.jp>
2020-01-28 23:31:39 +0900

    #179 「固定記事」プラグインの一時保存バッジの設置

--- resources/views/plugins/user/contents/default/contents.blade.php
+++ resources/views/plugins/user/contents/default/contents.blade.php
@@ -33,6 +33,9 @@
                 @endif
 
                 @can('posts.update',[[$contents, 'contents', $buckets]])
+                    @if ($contents->status == 1)
+                        <span class="badge badge-warning align-bottom">一時保存</span>
+                    @endif
                     <a href="{{url('/')}}/plugin/contents/edit/{{$page->id}}/{{$frame_id}}/{{$contents->id}}#frame-{{$frame_id}}">
                         <span class="btn btn-success btn-sm"><i class="far fa-edit"></i> <span class="hidden-xs">編集</span></span>
                     </a>
