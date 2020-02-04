28d24e4482c44d01323097a650121b6e120b86ac
masaton0216 <masamasamasato0216@gmail.com>
2020-02-04 17:05:18 +0900

    メニュー ディレクトリ展開式 デザイン用テンプレート追加

---
+++ resources/views/plugins/user/menus/opencurrenttree_for_design/menu_children.blade.php
@@ -0,0 +1,68 @@
+{{--
+ * メニューの子要素表示画面
+ *
+ * @param obj $pages ページデータの配列
+ * @author 永原　篤 <nagahara@opensource-workshop.jp>
+ * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
+ * @category メニュープラグイン
+--}}
+
+@php
+    // 下層ページに表示ONのものがあれば、カレントページに ＋ や ー を表示するため、
+    // 下層ページに表示ONのものがあるかチェックするための関数
+    $display_check = function ($pages) use(&$display_check) {
+        $child_on = false;
+        foreach ($pages as $page) {
+            if ($page->children) {
+                $child_on = $display_check($page->children);
+            }
+            if ($page->display_flag) {
+                $child_on = true;
+                return $child_on;
+            }
+        }
+        return $child_on;
+    };
+@endphp
+
+@if ($children->isView(Auth::user()))
+    <li>
+        @if ($children->id == $page_id)
+        <a href="{{$children->getUrl()}}" {!!$children->getUrlTargetTag()!!} class="list-group-item active">
+        @else
+        <a href="{{$children->getUrl()}}" {!!$children->getUrlTargetTag()!!} class="list-group-item">
+        @endif
+
+            {{-- 各ページの深さをもとにインデントの表現 --}}
+            @for ($i = 0; $i < $children->depth; $i++)
+                {{-- @if ($i+1==$children->depth && $menu) {!!$menu->getIndentFont()!!} @else <span class="px-2"></span>@endif --}}
+            @endfor
+
+        {{-- 子ページがある＆子ページに表示ON のページがある --}}
+        @if ($children->children && count($children->children) > 0 && $display_check($children->children))
+
+            {{-- カレントもしくは自分のルート筋なら表示する --}}
+            @if ($children->isAncestorOf($current_page) || $current_page->id == $children->id)
+                {{$children->page_name}} {!!$menu->getFolderCloseFont()!!}
+            @else
+                {{$children->page_name}} {!!$menu->getFolderOpenFont()!!}
+            @endif
+
+        @else
+            {{$children->page_name}}
+        @endif
+        </a>
+    </li>
+
+    @if ($children->children && count($children->children) > 0)
+
+        {{-- カレントもしくは自分のルート筋なら子ページを再帰的に表示する --}}
+        @if ($children->isAncestorOf($current_page) || $current_page->id == $children->id)
+            <ul>
+                @foreach($children->children as $grandchild)
+                    @include('plugins.user.menus.opencurrenttree_for_design.menu_children',['children' => $grandchild])
+                @endforeach
+            </ul>
+        @endif
+    @endif
+@endif
---
+++ resources/views/plugins/user/menus/opencurrenttree_for_design/menus.blade.php
@@ -0,0 +1,80 @@
+{{--
+ * メニュー表示画面
+ *
+ * @param obj $pages ページデータの配列
+ * @author 永原　篤 <nagahara@opensource-workshop.jp>
+ * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
+ * @category メニュープラグイン
+--}}
+@extends('core.cms_frame_base')
+
+@section("plugin_contents_$frame->id")
+@if ($pages)
+
+    <div class="list-group" style="margin-bottom: 0;">
+        <ul>
+            @foreach($pages as $page)
+
+                {{-- 子供のページがある場合 --}}
+                @if (count($page->children) > 0)
+
+                    {{-- 非表示のページは対象外(非表示の判断はこのページのみで、子のページはそのページでの判断を行う) --}}
+                    @if ($page->isView(Auth::user()))
+
+                        <li>
+                            {{-- リンク生成。メニュー項目全体をリンクにして階層はその中でインデント表記したいため、a タグから記載 --}}
+                            @if ($page->id == $page_id)
+                            <a href="{{$page->getUrl()}}" {!!$page->getUrlTargetTag()!!} class="list-group-item active">
+                            @else
+                            <a href="{{$page->getUrl()}}" {!!$page->getUrlTargetTag()!!} class="list-group-item">
+                            @endif
+                                {{-- 各ページの深さをもとにインデントの表現 --}}
+                                @for ($i = 0; $i < $page->depth; $i++)
+                                    {{-- @if ($i+1==$children->depth && $menu) {!!$menu->getIndentFont()!!} @else <span class="px-2"></span>@endif --}}
+                                @endfor
+                                {{$page->page_name}}
+
+                                {{-- カレントもしくは自分のルート筋なら＋、違えば－を表示する --}}
+                                @if ($page->isAncestorOf($current_page) || $current_page->id == $page->id)
+                                    {!!$menu->getFolderCloseFont()!!}
+                                @else
+                                    {!!$menu->getFolderOpenFont()!!}
+                                @endif
+                            </a>
+                        </li>
+                    @endif
+
+                    {{-- カレントもしくは自分のルート筋なら表示する --}}
+                    @if ($page->isAncestorOf($current_page) || $current_page->id == $page->id)
+                        <ul>
+                            {{-- 子要素を再帰的に表示するため、別ファイルに分けてinclude --}}
+                            @foreach($page->children as $children)
+                                @include('plugins.user.menus.opencurrenttree_for_design.menu_children',['children' => $children, 'page_id' => $page_id])
+                            @endforeach
+                        </ul>
+                    @endif
+                @else
+
+                    {{-- 非表示のページは対象外 --}}
+                    @if ($page->isView(Auth::user()))
+                        <li>
+                            {{-- リンク生成。メニュー項目全体をリンクにして階層はその中でインデント表記したいため、a タグから記載 --}}
+                            @if ($page->id == $page_id)
+                            <a href="{{$page->getUrl()}}" {!!$page->getUrlTargetTag()!!} class="list-group-item active">
+                            @else
+                            <a href="{{$page->getUrl()}}" {!!$page->getUrlTargetTag()!!} class="list-group-item">
+                            @endif
+                                {{-- 各ページの深さをもとにインデントの表現 --}}
+                                @for ($i = 0; $i < $page->depth; $i++)
+                                    {{-- @if ($i+1==$children->depth && $menu) {!!$menu->getIndentFont()!!} @else <span class="px-2"></span>@endif --}}
+                                @endfor
+                                {{$page->page_name}}
+                            </a>
+                        </li>
+                    @endif
+                @endif
+            @endforeach
+        </ul>
+    </div>
+@endif
+@endsection
