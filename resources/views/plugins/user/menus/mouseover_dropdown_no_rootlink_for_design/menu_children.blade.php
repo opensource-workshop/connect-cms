2a867b63be007087d6a64ee1a9c615f887534cff
masaton0216 <masamasamasato0216@gmail.com>
2020-02-04 14:03:39 +0900

    メニュー デザイン用のテンプレート追加

---
+++ resources/views/plugins/user/menus/mouseover_dropdown_no_rootlink_for_design/menu_children.blade.php
@@ -0,0 +1,32 @@
+{{--
+ * メニューの子要素表示画面
+ *
+ * @param obj $pages ページデータの配列
+ * @author 永原　篤 <nagahara@opensource-workshop.jp>
+ * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
+ * @category メニュープラグイン
+--}}
+
+@if ($children->isView(Auth::user()))
+<li>
+    @if ($children->id == $page_id)
+    <a class="dropdown-item active" href="{{$children->getUrl()}}" {!!$children->getUrlTargetTag()!!}>
+    @else
+    <a class="dropdown-item" href="{{$children->getUrl()}}" {!!$children->getUrlTargetTag()!!}>
+    @endif
+
+        {{-- 各ページの深さをもとにインデントの表現 --}}
+        {{-- @for ($i = 0; $i < $children->depth; $i++)
+            @if ($i+1==$children->depth) {!!$menu->getIndentFont()!!} @else <span class="px-2"></span>@endif
+        @endfor --}}
+        {{$children->page_name}}
+    </a>
+    @if ($children->children && count($children->children) > 0)
+        <ul>
+            @foreach($children->children as $ckey => $depth_children)
+                @include('plugins.user.menus.mouseover_dropdown_no_rootlink_for_design.menu_children',['children' => $children->children[$ckey]])
+            @endforeach
+        </ul>
+    @endif
+</li>
+@endif
---
+++ resources/views/plugins/user/menus/mouseover_dropdown_no_rootlink_for_design/menus.blade.php
@@ -0,0 +1,59 @@
+{{--
+ * メニュー表示画面
+ *
+ * @param obj $pages ページデータの配列
+ * @author 永原　篤 <nagahara@opensource-workshop.jp>
+ * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
+ * @category メニュープラグイン
+--}}
+@extends('core.cms_frame_base')
+
+@section("plugin_contents_$frame->id")
+@if ($pages)
+    <ul class="nav nav-tabs nav-justified d-none d-md-flex" style="">
+    @foreach($pages as $page_obj)
+
+        {{-- 非表示のページは対象外 --}}
+        @if ($page_obj->isView(Auth::user()))
+
+            {{-- 子供のページがある場合 --}}
+            @if (count($page_obj->children) > 0)
+
+                <li class="nav-item dropdown {{$page_obj->getClass()}}" onmouseleave="$(this).find('a.nav-link').click();$(this).find('a.nav-link').blur();">
+                {{-- カレント --}}
+                @if ($page_obj->id == $page_id)
+                    <a class="nav-link active dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" onmouseover="this.click();this.blur();">
+                @else
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" onmouseover="this.click();this.blur();">
+                @endif
+
+                        {{$page_obj->page_name}}
+                        <span class="caret"></span>
+                    </a>
+                    <div class="dropdown-menu">
+                        <ul>
+
+                        {{-- 自分へのリンク（ドロップダウンでリンクができなくなるため） --}}
+{{--
+                        <a class="dropdown-item" href="{{$page_obj->getUrl()}}" {!!$page_obj->getUrlTargetTag()!!}>{{$page_obj->page_name}}</a>
+                        <div class="dropdown-divider"></div>
+--}}
+                        {{-- 子要素を再帰的に表示するため、別ファイルに分けてinclude --}}
+                            @foreach($page_obj->children as $children)
+                                @include('plugins.user.menus.mouseover_dropdown_no_rootlink_for_design.menu_children',['children' => $children])
+                            @endforeach
+                        </ul>
+                    </div>
+                </li>
+            @else
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{$page_obj->getUrl()}}" {!!$page_obj->getUrlTargetTag()!!}>
+                        {{$page_obj->page_name}}
+                    </a>
+                </li>
+            @endif
+        @endif
+    @endforeach
+    </ul>
+@endif
+@endsection
