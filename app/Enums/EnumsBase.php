a7fdb49ca21dd8a98ad816b5f9d6c7a59108751a
Mitsuru Mutaguchi <akagane99@gmail.com>
2020-02-15 19:43:20 +0900

    add: Enums基底クラス追加

---
+++ app/Enums/EnumsBase.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Enums;
+
+/**
+ * Enums基底クラス
+ *
+ * 全てのEnumsの基底クラス
+ *
+ * @author 牟田口 満 <mutaguchi@opensource-workshop.jp>
+ * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
+ * @category 基底クラス
+ * @package Enums
+ */
+class EnumsBase
+{
+    // key/valueの連想配列
+    const enum = [];
+
+    /*
+    * 対応した和名を返す
+    */
+    public static function getDescription($key): string
+    {
+        return static::enum[$key];
+    }
+
+    /*
+    * key/valueの連想配列を返す
+    */
+    public static function getMembers(){
+        return static::enum;
+    }
+}
