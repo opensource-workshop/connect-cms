7f01b3384a2267271cc3eefeb3d25b50aa4372df
Mitsuru Mutaguchi <akagane99@gmail.com>
2020-02-19 16:08:30 +0900

    mod: UserableとUserableNohistoryの統合

--- app/Userable.php
+++ app/Userable.php
@@ -8,9 +8,9 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
 /**
- * 履歴ありUserable
  * モデルの保存時に自動的にユーザーID やユーザー名を保持するためのtrait
- * 承認などの履歴ありmodelに適用するtraitため、created_idを自動登録しない
+ * 履歴ありmodelに適用（承認など）: created_idを自動登録しない
+ * 履歴なしmodelに適用（コード管理など）: created_idを自動登録する
  * 
  * 使用するには、モデルでcreated_id、created_name、updated_id、updated_nameを定義してuseする。
  *
@@ -29,6 +29,10 @@ trait Userable
         static::creating(function (Model $model) {
             // created_idはデータ更新権限のチェックのため、最初に記事を書いたユーザのものを引き継ぐ必要があるので、自動登録はしない。
             // $model->created_id   = Auth::user()->id;
+            // 履歴ありモデル（テーブル）には status があり値は必ずセットされるはず。なければ履歴なしモデルなので、created_idを自動登録する。
+            if (is_null($model->status)){
+                $model->created_id   = Auth::user()->id;
+            }
             $model->created_name = Auth::user()->name;
         });
 
