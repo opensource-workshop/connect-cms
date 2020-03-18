5f8733a6aa7e53d36ca0e20ba2d34ab6c8f40604
オープンソース・ワークショップ 永原 篤 <nagahara@opensource-workshop.jp>
2020-03-18 15:36:14 +0900

    記事入力時の入力エラーでカテゴリの選択が外れる修正

--- resources/views/plugins/user/blogs/default/blogs_input.blade.php
+++ resources/views/plugins/user/blogs/default/blogs_input.blade.php
@@ -79,7 +79,7 @@
         <select class="form-control" name="categories_id" class="form-control">
             <option value=""></option>
             @foreach($blogs_categories as $category)
-            <option value="{{$category->id}}" @if(Input::old('category', $blogs_posts->categories_id)==$category->id) selected="selected" @endif>{{$category->category}}</option>
+            <option value="{{$category->id}}" @if(old('categories_id', $blogs_posts->categories_id)==$category->id) selected="selected" @endif>{{$category->category}}</option>
             @endforeach
         </select>
         @if ($errors && $errors->has('category')) <div class="text-danger">{{$errors->first('category')}}</div> @endif
