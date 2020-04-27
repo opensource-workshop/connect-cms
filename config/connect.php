19f15c19f74ec9dc761fff92393a7d940ef5e909
オープンソース・ワークショップ 永原 篤 <nagahara@opensource-workshop.jp>
2020-04-27 17:58:56 +0900

    ファイルの404, 403 への対応画像

--- config/connect.php
+++ config/connect.php
@@ -3,10 +3,10 @@
 return [
 
     // 画像がなかった場合の「no image」
-    'no_image_path' => 'app/uploads/default/no_image.png',
+    'no_image_path' => 'app/public/no_image.png',
 
     // 画像に権限がなかった場合の「forbidden」
-    'forbidden_image_path' => 'app/uploads/default/forbidden.png',
+    'forbidden_image_path' => 'app/public/forbidden.png',
 
     // プラグイン管理にも表示しないプラグイン(小文字で指定)
     'PLUGIN_FORCE_HIDDEN' => ['sampleforms', 'knowledges', 'codestudies'],
