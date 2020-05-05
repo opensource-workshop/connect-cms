39d86bbbffb558adc3e32cef899293c1d5ad7876
オープンソース・ワークショップ 永原 篤 <nagahara@opensource-workshop.jp>
2020-05-06 07:26:13 +0900

    データベースプラグインのカラム設定にClass名を追加

---
+++ database/migrations/2020_05_06_072115_add_class_databases_columns.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddClassDatabasesColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('databases_columns', function (Blueprint $table) {
+            //
+            $table->string('classname')->nullable()->comment('Class名')->after('select_flag');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('classname', function (Blueprint $table) {
+            //
+        });
+    }
+}
