63cb018b90122a4b1725949c69215964d2e6a420
Masato Inoue <masamasamasato0216@gmail.com>
2020-06-05 10:55:14 +0900

    タブ 設定画面 参照できるフレームがない状態で更新ボタン押すとエラー対応＆導線メッセージ追加

--- resources/views/plugins/user/tabs/default/tabs_select.blade.php
+++ resources/views/plugins/user/tabs/default/tabs_select.blade.php
@@ -16,7 +16,9 @@
 <form action="{{url('/')}}/plugin/tabs/saveSelect/{{$page->id}}/{{$frame->frame_id}}#frame-{{$frame->id}}" name="tabs_form" method="POST">
     {{ csrf_field() }}
 
-    @if ($frames)
+    @if ($frames && count($frames) > 0)
+    {{-- 同ページに参照できる別フレームがあれば設定画面を表示 --}}
+
     <table class="mt-3">
         <tr>
             <th class="pr-3">初期選択</th>
@@ -56,17 +58,28 @@
         </tr>
         @endforeach
     </table>
+    @else
+        {{-- 同ページに参照できる別フレームがなければワーニングメッセージ表示 --}}
+        <div class="alert alert-warning" style="margin-top: 10px;">
+            <i class="fas fa-exclamation-circle"></i>
+            参照できるコンテンツがありません。同ページに他のプラグインでコンテンツを設置してください。
+        </div>
     @endif
 
     <div class="form-group text-center mt-3">
         <div class="row">
             <div class="col-12">
+                {{-- キャンセルボタン --}}
                 <button type="button" class="btn btn-secondary form-horizontal mr-2" onclick="location.href='{{URL::to($page->permanent_link)}}'">
                     <i class="fas fa-times"></i> キャンセル
                 </button>
-                <button type="submit" class="btn btn-primary form-horizontal">
-                    <i class="fas fa-check"></i> 更新
-                </button>
+                {{-- 同ページに参照できる別フレームがあれば更新ボタンを表示 --}}
+                @if ($frames && count($frames) > 0)
+                    {{-- 更新ボタン --}}
+                    <button type="submit" class="btn btn-primary form-horizontal">
+                        <i class="fas fa-check"></i> 更新
+                    </button>
+                @endif
             </div>
         </div>
     </div>
