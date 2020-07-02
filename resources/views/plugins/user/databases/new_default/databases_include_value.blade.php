d224cf9c98d74f9a3f1390130c1180459abd9f8c
Mitsuru Mutaguchi <akagane99@gmail.com>
2020-07-02 10:08:18 +0900

    add: database plugin, add new_default template 単票形式

---
+++ resources/views/plugins/user/databases/new_default/databases_include_value.blade.php
@@ -0,0 +1,60 @@
+{{--
+ * データ表示用テンプレート。
+ *
+ * @author 永原　篤 <nagahara@opensource-workshop.jp>
+ * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
+ * @category データベース・プラグイン
+--}}
+@php
+    $obj = $input_cols->where('databases_inputs_id', $input->id)->where('databases_columns_id', $column->id)->first();
+
+    // ファイル型
+    if ($column->column_type == 'file') {
+        if (empty($obj)) {
+            $value = '';
+        }
+        else {
+            $value = '<a href="' . url('/') . '/file/' . $obj->value . '" target="_blank">' . $obj->client_original_name . '</a>';
+        }
+    }
+    // 画像型
+    else if ($column->column_type == 'image') {
+        if (empty($obj)) {
+            $value = '';
+        }
+        else {
+            $value = '<img src="' . url('/') . '/file/' . $obj->value . '" class="img-fluid" />';
+        }
+    }
+    // 動画型
+    else if ($column->column_type == 'video') {
+        if (empty($obj)) {
+            $value = '';
+        }
+        else {
+            $value = '<video src="' . url('/') . '/file/' . $obj->value . '" class="img-fluid" controls />';
+        }
+    }
+    // その他の型
+    else {
+        $value = $obj ? $obj->value : "";
+    }
+
+    // custom: 空の場合、なにか出力しないと「項目名<br>値」で出力してる関係で高さがずれる。
+    if (is_null($value) || $value === '') {
+        $value = "&nbsp;";
+    }
+@endphp
+
+{{-- ファイル型 --}}
+@if ($column->column_type == 'file')
+    {!!$value!!}
+@elseif ($column->column_type == 'image')
+    {!!$value!!}
+@elseif ($column->column_type == 'video')
+    {!!$value!!}
+@elseif ($column->column_type == 'wysiwyg')
+    {!!$value!!}
+@else
+    {!!nl2br(e($value))!!}
+@endif
