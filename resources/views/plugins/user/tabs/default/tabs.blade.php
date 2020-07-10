e358ea190a1303c14bc5398e59949a444bac04bc
Mitsuru Mutaguchi <akagane99@gmail.com>
2020-07-10 11:48:04 +0900

    bugfix: frame_titleが空の場合、タブが表示されないため、メッセージをタブに表示する https://github.com/opensource-workshop/connect-cms/issues/411

--- resources/views/plugins/user/tabs/default/tabs.blade.php
+++ resources/views/plugins/user/tabs/default/tabs.blade.php
@@ -3,6 +3,7 @@
  *
  * @param obj $pages ページデータの配列
  * @author 永原　篤 <nagahara@opensource-workshop.jp>
+ * @author 牟田口 満 <mutaguchi@opensource-workshop.jp>
  * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
  * @category タブ・プラグイン
 --}}
@@ -11,7 +12,11 @@
 @section("plugin_contents_$frame->id")
 <ul class="nav nav-tabs">
 @foreach($frames as $frame_record)
-    <li id="tab_{{$frame_record->id}}" class="tab_{{$frame_record->id}} nav-item"><a href="#" class="nav-link tab_a_{{$frame_record->id}} @if (isset($tabs) && $tabs->default_frame_id == $frame_record->id) active @endif" onclick="return false;">{{$frame_record->frame_title}}</a></li>
+    @php
+    // frame_titleが空の場合、タブが表示されないため、メッセージをタブに表示する
+    $frame_title = is_null($frame_record->frame_title) ? '(フレームタイトルを設定してください)' : $frame_record->frame_title;
+    @endphp
+    <li id="tab_{{$frame_record->id}}" class="tab_{{$frame_record->id}} nav-item"><a href="#" class="nav-link tab_a_{{$frame_record->id}} @if (isset($tabs) && $tabs->default_frame_id == $frame_record->id) active @endif" onclick="return false;">{{  $frame_title  }}</a></li>
 @endforeach
 </ul>
 
