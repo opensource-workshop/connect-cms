400f30ee533b2c5201e13a9f5a1724441ae24444
Masato Inoue <masamasamasato0216@gmail.com>
2020-07-20 11:38:28 +0900

    フォーム 半角数値指定時の丸め処理をマイナス入力時も対応

--- app/Plugins/User/Forms/FormsPlugin.php
+++ app/Plugins/User/Forms/FormsPlugin.php
@@ -312,11 +312,23 @@ Mail::to('nagahara@osws.jp')->send(new ConnectMail($content));
         // 数値チェック
         if ($forms_column->rule_allowed_numeric) {
             if ($request->forms_columns_value[$forms_column->id]) {
-                // 入力値があった場合
+                // 入力値があった場合（マイナスを意図した入力記号はすべて半角に置換する）
+                $replace_defs = [
+                    'ー' => '-',
+                    '－' => '-',
+                    '―' => '-'
+                ];
+                $search = array_keys($replace_defs);
+                $replace = array_values($replace_defs);
+
                 if (
                         is_numeric(
                             mb_convert_kana(
-                                $request->forms_columns_value[$forms_column->id], 
+                                str_replace(
+                                    $search, 
+                                    $replace, 
+                                    $request->forms_columns_value[$forms_column->id]
+                                ), 
                                 'n'
                             )
                         )
@@ -326,7 +338,11 @@ Mail::to('nagahara@osws.jp')->send(new ConnectMail($content));
                     // 全角→半角へ丸める
                     $tmp_array[$forms_column->id] = 
                         mb_convert_kana(
-                            $request->forms_columns_value[$forms_column->id], 
+                            str_replace(
+                                $search, 
+                                $replace, 
+                                $request->forms_columns_value[$forms_column->id]
+                            ), 
                             'n'
                         );
                     $request->merge([
