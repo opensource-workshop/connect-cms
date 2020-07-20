9159709b87c5e7ffb826dc9453e936b49b399a33
Masato Inoue <masamasamasato0216@gmail.com>
2020-07-20 15:14:38 +0900

    フォーム テキストエリアの行幅を広めに

--- resources/views/plugins/user/forms/default/forms_input_textarea.blade.php
+++ resources/views/plugins/user/forms/default/forms_input_textarea.blade.php
@@ -5,7 +5,7 @@
  * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
  * @category フォーム・プラグイン
  --}}
-<textarea name="forms_columns_value[{{$form_obj->id}}]" class="form-control">{{old('forms_columns_value.'.$form_obj->id, $request->forms_columns_value[$form_obj->id])}}</textarea>
+<textarea rows="4" name="forms_columns_value[{{$form_obj->id}}]" class="form-control">{{old('forms_columns_value.'.$form_obj->id, $request->forms_columns_value[$form_obj->id])}}</textarea>
 @if ($errors && $errors->has("forms_columns_value.$form_obj->id"))
     <div class="text-danger"><i class="fas fa-exclamation-circle"></i> {{$errors->first("forms_columns_value.$form_obj->id")}}</div>
 @endif
