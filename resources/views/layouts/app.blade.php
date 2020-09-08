461661d5fd0fc09a9c3d8db648438f72578a13bf
Masato Inoue <masamasamasato0216@gmail.com>
2020-09-08 16:49:05 +0900

    メッセージ管理 コア側bladeに初回確認メッセージモーダル追加 ※cookie未対応

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -338,5 +338,44 @@
     @endif
     @endauth
 
+    {{-- 初回確認メッセージの表示モーダル --}}
+    @php
+        // Configsテーブルから設定値を取得
+        $message_first_show_type = isset($configs_array['message_first_show_type']) ? $configs_array['message_first_show_type']->value : 0;
+        $message_first_permission_type = isset($configs_array['message_first_permission_type']) ? $configs_array['message_first_permission_type']->value : 0;
+        $message_first_exclued_urls = isset($configs_array['message_first_exclued_url']) ? explode(',' ,$configs_array['message_first_exclued_url']->value) : array();
+    @endphp
+    {{-- 管理画面で設定ON、且つ、本ページが初回確認メッセージ表示の除外URL以外の場合にメッセージ表示 --}}
+    @if($message_first_show_type == ShowType::show && !in_array($page->permanent_link ,$message_first_exclued_urls))
+
+        <!-- 初回確認メッセージ表示用のモーダルウィンドウ -->
+        <div class="modal" id="first_message_modal" tabindex="-1" role="dialog" aria-labelledby="first_message_modal_label" aria-hidden="true" data-backdrop="{{ $message_first_permission_type == PermissionType::not_allowed ? 'static' : 'true' }}">
+            <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        {{-- メッセージ内容 --}}
+                        {!! $configs_array['message_first_content']->value !!}
+                    </div>
+                    <div class="modal-footer">
+                        <form action="{{url('/core/frame/addCookieForFirstMessage')}}/{{$page->id}}" name="form_add_cookie_first_message" method="POST">
+                            {{ csrf_field() }}
+
+                            {{-- ボタン --}}
+                            <button type="button" class="btn btn-primary" data-dismiss="modal">
+                                {{ $configs_array['message_first_button_name']->value }}
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script>
+            // 画面ロード時にモーダル表示
+            window.onload = function() {
+                $('#first_message_modal').modal();
+            };
+        </script>
+    @endif
+
 </body>
 </html>
