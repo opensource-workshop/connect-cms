1f62a50e4252bf3d6f5c5bd965e1efe645359252
Mitsuru Mutaguchi <akagane99@gmail.com>
2020-09-16 14:46:27 +0900

    add: database plugin, dbカラムのシステム項目対応

--- app/Models/User/Databases/DatabasesInputCols.php
+++ app/Models/User/Databases/DatabasesInputCols.php
@@ -4,8 +4,13 @@ namespace App\Models\User\Databases;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\UserableNohistory;
+
 class DatabasesInputCols extends Model
 {
+    // 保存時のユーザー関連データの保持（履歴なしUserable）
+    use UserableNohistory;
+
     // 更新する項目の定義
     protected $fillable = ['databases_inputs_id', 'databases_columns_id', 'value'];
 }
