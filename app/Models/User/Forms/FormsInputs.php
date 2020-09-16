294bfd69caaccf98ab4d46fea5d64767dcaa7639
Mitsuru Mutaguchi <akagane99@gmail.com>
2020-09-16 15:07:54 +0900

    add: 【フォーム】dbカラムのシステム項目対応 https://github.com/opensource-workshop/connect-cms/issues/559

--- app/Models/User/Forms/FormsInputs.php
+++ app/Models/User/Forms/FormsInputs.php
@@ -4,8 +4,13 @@ namespace App\Models\User\Forms;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\UserableNohistory;
+
 class FormsInputs extends Model
 {
+    // 保存時のユーザー関連データの保持（履歴なしUserable）
+    use UserableNohistory;
+
     // 更新する項目の定義
     protected $fillable = ['forms_id'];
 }
