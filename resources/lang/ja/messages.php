b395afe3fbbaa43ebad641e9f823e89c66890c57
Masato Inoue <masamasamasato0216@gmail.com>
2020-09-30 10:14:56 +0900

    フォーム 時間型FromTo項目のエラーメッセージが英語onlyだった

--- resources/lang/ja/messages.php
+++ resources/lang/ja/messages.php
@@ -29,6 +29,7 @@ return [
     'enter_same_email' => '同じメールアドレスを入力',
     'not_match_confirmation_value' => 'が確認用の値と一致しません。',
     'entered_time_is_invalid' => '入力した時間の前後関係が不正です。',
+    'both_required' => '両方の項目を入力してください。',
     'search_results' => '検索結果',
     'cases' => '件',
 ];
