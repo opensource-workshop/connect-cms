3879de4b11d6527a97e0089c0d344256d809db8d
オープンソース・ワークショップ 永原 篤 <nagahara@opensource-workshop.jp>
2020-10-02 19:15:07 +0900

    作業区切り

---
+++ resources/views/plugins/user/conventions/default/bucket.blade.php
@@ -0,0 +1,94 @@
+{{--
+ * イベント・バケツ編集画面テンプレート。
+ *
+ * @author 永原　篤 <nagahara@opensource-workshop.jp>
+ * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
+ * @category イベント管理プラグイン
+ --}}
+@extends('core.cms_frame_base_setting')
+
+@section("core.cms_frame_edit_tab_$frame->id")
+    {{-- プラグイン側のフレームメニュー --}}
+    @include('plugins.user.conventions.conventions_frame_edit_tab')
+@endsection
+
+@section("plugin_setting_$frame->id")
+@if (empty($convention->id) && $action != 'createBuckets')
+    <div class="alert alert-warning">
+        <i class="fas fa-exclamation-circle"></i>
+        選択画面から、使用するイベントを選択するか、作成してください。
+    </div>
+@else
+    <div class="alert alert-info">
+        <i class="fas fa-exclamation-circle"></i>
+        @if (empty($convention->id))
+            新しいイベント設定を登録します。
+        @else
+            イベント設定を変更します。
+        @endif
+    </div>
+
+    @if (empty($convention->id))
+    <form action="{{url('/')}}/redirect/plugin/conventions/saveBuckets/{{$page->id}}/{{$frame_id}}#frame-{{$frame->id}}" method="POST" class="">
+        <input type="hidden" name="redirect_path" value="{{url('/')}}/plugin/conventions/createBuckets/{{$page->id}}/{{$frame_id}}#frame-{{$frame_id}}">
+    @else
+    <form action="{{url('/')}}/redirect/plugin/conventions/saveBuckets/{{$page->id}}/{{$frame_id}}/{{$convention->bucket_id}}#frame-{{$frame->id}}" method="POST" class="">
+        <input type="hidden" name="redirect_path" value="{{url('/')}}/plugin/conventions/editBuckets/{{$page->id}}/{{$frame_id}}#frame-{{$frame_id}}">
+    @endif
+        {{ csrf_field() }}
+        <div class="form-group row">
+            <label class="{{$frame->getSettingLabelClass()}}">イベント名 <label class="badge badge-danger">必須</label></label>
+            <div class="{{$frame->getSettingInputClass()}}">
+                <input type="text" name="name" value="{{old('name', $convention->name)}}" class="form-control">
+                @if ($errors && $errors->has('name')) <div class="text-danger">{{$errors->first('name')}}</div> @endif
+            </div>
+        </div>
+        {{-- Submitボタン --}}
+        <div class="form-group text-center">
+            <div class="row">
+                <div class="col-3"></div>
+                <div class="col-6">
+                    <button type="button" class="btn btn-secondary mr-2" onclick="location.href='{{URL::to($page->permanent_link)}}'">
+                        <i class="fas fa-times"></i><span class="{{$frame->getSettingButtonCaptionClass('md')}}"> キャンセル</span>
+                    </button>
+                    <button type="submit" class="btn btn-primary form-horizontal"><i class="fas fa-check"></i> 
+                        <span class="{{$frame->getSettingButtonCaptionClass()}}">
+                        @if (empty($convention->id))
+                            登録確定
+                        @else
+                            変更確定
+                        @endif
+                        </span>
+                    </button>
+                </div>
+
+                {{-- 既存イベントの場合は削除処理のボタンも表示 --}}
+                @if (!empty($convention->id))
+                <div class="col-3 text-right">
+                    <a data-toggle="collapse" href="#collapse{{$frame->id}}">
+                        <span class="btn btn-danger"><i class="fas fa-trash-alt"></i><span class="{{$frame->getSettingButtonCaptionClass()}}"> 削除</span></span>
+                    </a>
+                </div>
+                @endif
+            </div>
+        </div>
+    </form>
+
+    <div id="collapse{{$frame->id}}" class="collapse">
+        <div class="card border-danger">
+            <div class="card-body">
+                <span class="text-danger">イベントを削除します。<br>このイベントに記載した詳細も削除され、元に戻すことはできないため、よく確認して実行してください。</span>
+
+                <div class="text-center">
+                    {{-- 削除ボタン --}}
+                    <form action="{{url('/')}}/redirect/plugin/conventions/destroyBuckets/{{$page->id}}/{{$frame_id}}/{{$convention->id}}#frame-{{$frame->id}}" method="POST">
+                        {{csrf_field()}}
+                        <button type="submit" class="btn btn-danger" onclick="javascript:return confirm('データを削除します。\nよろしいですか？')"><i class="fas fa-check"></i> 本当に削除する</button>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+@endif
+@endsection
---
+++ resources/views/plugins/user/conventions/default/index.blade.php
@@ -0,0 +1,91 @@
+{{--
+ * イベント画面テンプレート。
+ *
+ * @author 永原　篤 <nagahara@opensource-workshop.jp>
+ * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
+ * @category イベント管理プラグイン
+ --}}
+@extends('core.cms_frame_base')
+
+@section("plugin_contents_$frame->id")
+
+@if (isset($frame) && $frame->bucket_id)
+    {{-- バケツあり --}}
+@else
+    {{-- バケツなし --}}
+    <div class="card border-danger">
+        <div class="card-body">
+            <p class="text-center cc_margin_bottom_0">フレームの設定画面から、使用するイベントを選択するか、作成してください。</p>
+        </div>
+    </div>
+@endif
+
+{{-- 新規登録 --}}
+@can('posts.create',[[null, 'conventions', $buckets]])
+    @if (isset($frame) && $frame->bucket_id)
+        <div class="row">
+            <p class="text-right col-12">
+                {{-- 新規登録ボタン --}}
+                <button type="button" class="btn btn-success" onclick="location.href='{{url('/')}}/plugin/conventions/edit/{{$page->id}}/{{$frame_id}}#frame-{{$frame_id}}'"><i class="far fa-edit"></i> 新規登録</button>
+            </p>
+        </div>
+    @endif
+@endcan
+
+{{-- リンク表示 --}}
+@if (isset($posts))
+    @if (!$plugin_frame->type)
+    <dl>
+    @elseif ($plugin_frame->type == 1)
+    <ul type="disc">
+    @elseif ($plugin_frame->type == 2)
+    <ul type="circle">
+    @elseif ($plugin_frame->type == 3)
+    <ul type="square">
+    @elseif ($plugin_frame->type == 4)
+    <ol type="1">
+    @elseif ($plugin_frame->type == 5)
+    <ol type="a">
+    @elseif ($plugin_frame->type == 6)
+    <ol type="A">
+    @elseif ($plugin_frame->type == 7)
+    <ol type="i">
+    @elseif ($plugin_frame->type == 8)
+    <ol type="I">
+    @endif
+    @foreach($posts as $post)
+        @if (!$plugin_frame->type)
+        <dd>
+        @else
+        <li>
+        @endif
+            @can('posts.update',[[null, 'conventions', $buckets]])
+                <a href="{{url('/')}}/plugin/conventions/edit/{{$page->id}}/{{$frame_id}}/{{$post->id}}#frame-{{$frame_id}}"><i class="far fa-edit"></a></i>
+            @endcan
+            @if (empty($post->url))
+                {{$post->title}}
+            @else
+                <a href="{{$post->url}}">{{$post->title}}</a>
+            @endif
+            @if (!empty($post->description))
+                <div class="alert alert-secondary bg-light mt-2" role="alert">
+                  {!!nl2br(e($post->description))!!}
+                </div>
+            @endif
+        @if (!$plugin_frame->type)
+        </dd>
+        @else
+        </li>
+        @endif
+    @endforeach
+    </ul>
+    @if (!$plugin_frame->type)
+    </dl>
+    @elseif ($plugin_frame->type == 1 || $plugin_frame->type == 2 || $plugin_frame->type == 3)
+    </ul>
+    @elseif ($plugin_frame->type == 4 || $plugin_frame->type == 5 || $plugin_frame->type == 6 || $plugin_frame->type == 7 || $plugin_frame->type == 8)
+    </ol>
+    @endif
+@endif
+
+@endsection
