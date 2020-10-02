f8acee2482c5157ad01b656cbc09522c692d132b
Mitsuru Mutaguchi <akagane99@gmail.com>
2020-10-02 10:59:33 +0900

    change: form, textテンプレート, 余計な空白削除

--- resources/views/plugins/user/forms/default/forms_input_text.blade.php
+++ resources/views/plugins/user/forms/default/forms_input_text.blade.php
@@ -4,7 +4,7 @@
  * @author 永原　篤 <nagahara@opensource-workshop.jp>
  * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
  * @category フォーム・プラグイン
- --}}
+--}}
 <input name="forms_columns_value[{{$form_obj->id}}]" class="form-control" type="{{$form_obj->column_type}}" value="@if ($frame_id == $request->frame_id){{old('forms_columns_value.'.$form_obj->id, $request->forms_columns_value[$form_obj->id])}}@endif">
 @if ($errors && $errors->has("forms_columns_value.$form_obj->id"))
     <div class="text-danger"><i class="fas fa-exclamation-circle"></i> {{$errors->first("forms_columns_value.$form_obj->id")}}</div>
