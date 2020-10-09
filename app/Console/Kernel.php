08295e4a220dfc5d39664fd56105382ca7274ea6
Masato Inoue <masamasamasato0216@gmail.com>
2020-10-09 11:20:40 +0900

    共通 kernel.phpに商用バッチ用のディレクトリを追加

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -40,6 +40,7 @@ class Kernel extends ConsoleKernel
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__.'/CommandsOption');
 
         require base_path('routes/console.php');
     }
