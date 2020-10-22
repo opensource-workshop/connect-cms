36549acfe022c194570c0b3a95eb7ff3379de09c
Mitsuru Mutaguchi <akagane99@gmail.com>
2020-10-22 11:12:42 +0900

    change: FAQ、一時保存、承認待ちが一覧でクリックしないと見れなかったため、タイトル横に追加表示 change: FAQのアクセシビリティ対応。<article>タグの中に<header>タグ、<footer>タグ追加 change: FAQのアクセシビリティ対応。ページング箇所、一覧へ戻る箇所は<nav>タグに変更 https://github.com/opensource-workshop/connect-cms/issues/610

--- resources/views/plugins/user/faqs/default/faqs.blade.php
+++ resources/views/plugins/user/faqs/default/faqs.blade.php
@@ -40,9 +40,9 @@
 @if (isset($faqs_posts))
     <div class="accordion" id="accordionFaq{{$frame_id}}">
     @foreach($faqs_posts as $post)
-        <div class="card">
+        <article class="card">
             <button class="btn btn-link p-0 text-left" type="button" data-toggle="collapse" data-target="#collapseFaq{{$post->id}}" aria-expanded="true" aria-controls="collapseFaq{{$post->id}}">
-                <div class="faq-list-title" id="headingFaq{{$post->id}}">
+                <header class="faq-list-title" id="headingFaq{{$post->id}}">
 
                     <div class="d-flex flex-row">
                         <div class="pr-2"><span class="h5"><span class="badge badge-primary">Q</span></span></div>
@@ -54,17 +54,26 @@
                             @if($post->category)
                                 <span class="badge" style="color:{{$post->category_color}};background-color:{{$post->category_background_color}};">{{$post->category}}</span>
                             @endif
+
+                            @if ($post->status == 2)
+                                @can('role_update_or_approval',[[$post, 'faqs', $buckets]])
+                                    <span class="badge badge-warning">承認待ち</span>
+                                @endcan
+                            @endif
+                            @can('posts.update',[[$post, 'faqs', $buckets]])
+                                @if ($post->status == 1)
+                                    <span class="badge badge-warning">一時保存</span>
+                                @endif
+                            @endcan
                         </div>
                     </div>
 
-                </div>
+                </header>
             </button>
 
+            {{-- 記事本文 --}}
             <div id="collapseFaq{{$post->id}}" class="collapse" aria-labelledby="headingFaq{{$post->id}}" data-parent="#accordionFaq{{$frame_id}}">
                 <div class="card-body">
-
-                {{-- 記事本文 --}}
-                <article>
 
                     <div class="d-flex flex-row">
                         <div class="pr-2"><span class="h5"><span class="badge badge-secondary">A</span></span></div>
@@ -74,70 +83,70 @@
                         </div>
                     </div>
 
-                    <div class="pt-1">
-                        {{-- 投稿日時 --}}
-                        公開日時：{{$post->posted_at->format('Y年n月j日 H時i分')}}
+                    <footer>
+                        <div class="pt-1">
+                            {{-- 投稿日時 --}}
+                            公開日時：{{$post->posted_at->format('Y年n月j日 H時i分')}}
 
-                        {{-- 重要記事 --}}
-                        @if($post->important == 1)
-                            <span class="badge badge-danger">重要</span>
-                        @endif
-
-                        {{-- カテゴリ --}}
-                        @if($post->category)
-                            <span class="badge" style="color:{{$post->category_color}};background-color:{{$post->category_background_color}};">{{$post->category}}</span>
-                        @endif
-
-                        {{-- タグ --}}
-                        @isset($post->tags)
-                            @foreach($post->tags as $tag)
-                                <span class="badge badge-secondary">{{$tag}}</span>
-                            @endforeach
-                        @endisset
-                    </div>
-
-                </article>
-
-                {{-- post データは以下のように2重配列で渡す（Laravelが配列の0番目のみ使用するので） --}}
-                <div class="row pt-2">
-                    <div class="col text-right mb-1">
-                        @if ($post->status == 2)
-                            @can('role_update_or_approval',[[$post, 'faqs', $buckets]])
-                                <span class="badge badge-warning align-bottom">承認待ち</span>
-                            @endcan
-                            @can('posts.approval',[[$post, 'faqs', $buckets]])
-                                <form action="{{url('/')}}/plugin/faqs/approval/{{$page->id}}/{{$frame_id}}/{{$post->id}}#frame-{{$frame->id}}" method="post" name="form_approval" class="d-inline">
-                                    {{ csrf_field() }}
-                                    <button type="submit" class="btn btn-primary btn-sm" onclick="javascript:return confirm('承認します。\nよろしいですか？');">
-                                        <i class="fas fa-check"></i> <span class="hidden-xs">承認</span>
-                                    </button>
-                                </form>
-                            @endcan
-                        @endif
-                        @can('posts.update',[[$post, 'faqs', $buckets]])
-                            @if ($post->status == 1)
-                                <span class="badge badge-warning align-bottom">一時保存</span>
+                            {{-- 重要記事 --}}
+                            @if($post->important == 1)
+                                <span class="badge badge-danger">重要</span>
                             @endif
-                            <a href="{{url('/')}}/plugin/faqs/edit/{{$page->id}}/{{$frame_id}}/{{$post->id}}#frame-{{$frame->id}}">
-                                <span class="btn btn-success btn-sm"><i class="far fa-edit"></i> <span class="hidden-xs">編集</span></span>
-                            </a>
-                        @endcan
 
-                        {{-- 詳細画面 --}}
-                        <a class="ml-2" href="{{url('/')}}/plugin/faqs/show/{{$page->id}}/{{$frame_id}}/{{$post->id}}#frame-{{$frame->id}}">
-                            <span class="btn btn-success btn-sm">詳細 <i class="fas fa-angle-right"></i></span>
-                        </a>
-                    </div>
+                            {{-- カテゴリ --}}
+                            @if($post->category)
+                                <span class="badge" style="color:{{$post->category_color}};background-color:{{$post->category_background_color}};">{{$post->category}}</span>
+                            @endif
+
+                            {{-- タグ --}}
+                            @isset($post->tags)
+                                @foreach($post->tags as $tag)
+                                    <span class="badge badge-secondary">{{$tag}}</span>
+                                @endforeach
+                            @endisset
+                        </div>
+
+                        {{-- post データは以下のように2重配列で渡す（Laravelが配列の0番目のみ使用するので） --}}
+                        <div class="row pt-2">
+                            <div class="col text-right mb-1">
+                                @if ($post->status == 2)
+                                    @can('role_update_or_approval',[[$post, 'faqs', $buckets]])
+                                        <span class="badge badge-warning align-bottom">承認待ち</span>
+                                    @endcan
+                                    @can('posts.approval',[[$post, 'faqs', $buckets]])
+                                        <form action="{{url('/')}}/plugin/faqs/approval/{{$page->id}}/{{$frame_id}}/{{$post->id}}#frame-{{$frame->id}}" method="post" name="form_approval" class="d-inline">
+                                            {{ csrf_field() }}
+                                            <button type="submit" class="btn btn-primary btn-sm" onclick="javascript:return confirm('承認します。\nよろしいですか？');">
+                                                <i class="fas fa-check"></i> <span class="hidden-xs">承認</span>
+                                            </button>
+                                        </form>
+                                    @endcan
+                                @endif
+                                @can('posts.update',[[$post, 'faqs', $buckets]])
+                                    @if ($post->status == 1)
+                                        <span class="badge badge-warning align-bottom">一時保存</span>
+                                    @endif
+                                    <a href="{{url('/')}}/plugin/faqs/edit/{{$page->id}}/{{$frame_id}}/{{$post->id}}#frame-{{$frame->id}}">
+                                        <span class="btn btn-success btn-sm"><i class="far fa-edit"></i> <span class="hidden-xs">編集</span></span>
+                                    </a>
+                                @endcan
+
+                                {{-- 詳細画面 --}}
+                                <a class="ml-2" href="{{url('/')}}/plugin/faqs/show/{{$page->id}}/{{$frame_id}}/{{$post->id}}#frame-{{$frame->id}}">
+                                    <span class="btn btn-success btn-sm">詳細 <i class="fas fa-angle-right"></i></span>
+                                </a>
+                            </div>
+                        </div>
+                    </footer>
                 </div>
             </div>
-        </div>
-    </div>
+        </article>
     @endforeach
     </div>
 
     {{-- ページング処理 --}}
-    <div class="text-center mt-3">
-        {{ $faqs_posts->links() }}
-    </div>
+    <nav class="text-center mt-3">
+        {{ $faqs_posts->fragment('frame-' . $frame_id)->links() }}
+    </nav>
 @endif
 @endsection
