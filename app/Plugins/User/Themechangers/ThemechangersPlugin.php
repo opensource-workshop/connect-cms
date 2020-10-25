fe0dd39ef94b950260c56e3066e8dc8ea88e02d5
Mitsuru Mutaguchi <akagane99@gmail.com>
2020-10-25 10:50:31 +0900

    add: 【各プラグイン】declareRoleファンクションのみ追加。今後、適切な追加の権限定義を設定する https://github.com/opensource-workshop/connect-cms/issues/658

--- app/Plugins/User/Themechangers/ThemechangersPlugin.php
+++ app/Plugins/User/Themechangers/ThemechangersPlugin.php
@@ -38,6 +38,20 @@ class ThemechangersPlugin extends UserPluginBase
     }
 
     /**
+     * 追加の権限定義（コアから呼び出す）
+     */
+    public function declareRole()
+    {
+        // 標準権限以外で設定画面などから呼ばれる権限の定義
+        // 標準権限は右記で定義 config/cc_role.php
+        //
+        // 権限チェックテーブル
+        // [TODO] 【各プラグイン】declareRoleファンクションで適切な追加の権限定義を設定する https://github.com/opensource-workshop/connect-cms/issues/658
+        $role_ckeck_table = array();
+        return $role_ckeck_table;
+    }
+
+    /**
      *  ページのカラム取得
      */
     private function getPagesColum($col_name)
