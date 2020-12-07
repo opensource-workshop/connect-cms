6c4bb45b05ee485e7b6de6ce1785af64d4445f54
Masato Inoue <masamasamasato0216@gmail.com>
2020-12-07 11:49:37 +0900

    ユーザ登録 登録時のログインID重複チェックを追加

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,6 +53,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => 'required|string|max:255',
             // 'email' => 'required|string|email|max:255|unique:users',
+            'userid' => 'required|max:255|unique:users',
             'email' => 'nullable|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
             'status' => 'required',
