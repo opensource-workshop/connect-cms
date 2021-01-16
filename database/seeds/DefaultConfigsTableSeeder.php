2f01b5ff7a002722c6be81a7c505173e98afbb15
Mitsuru Mutaguchi <akagane99@gmail.com>
2021-01-16 10:07:18 +0900

    butfix: 追加configsは、マイグレーションではなくseederで設定する。そうしないと、新規インストール時に初期configがインストールされなくなる https://github.com/opensource-workshop/connect-cms/issues/703

--- database/seeds/DefaultConfigsTableSeeder.php
+++ database/seeds/DefaultConfigsTableSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 
+use App\Models\Core\Configs;
+
 class DefaultConfigsTableSeeder extends Seeder
 {
     /**
@@ -93,6 +95,33 @@ class DefaultConfigsTableSeeder extends Seeder
                     ],
                 ]
             );
+        }
+
+        if (DB::table('configs')->where('name', 'base_login_password_reset')->count() == 0) {
+            // パスワードリセットの使用
+            $configs = Configs::create([
+                'name' => 'base_login_password_reset',
+                'category' => 'general',
+                'value' => 1
+            ]);
+        }
+
+        if (DB::table('configs')->where('name', 'use_mypage')->count() == 0) {
+            // マイページの使用
+            $configs = Configs::create([
+                'name' => 'use_mypage',
+                'category' => 'general',
+                'value' => 1
+            ]);
+        }
+
+        if (DB::table('configs')->where('name', 'use_normal_login_along_with_auth_method')->count() == 0) {
+            // 外部認証と併せて、通常ログインも使用
+            $configs = Configs::create([
+                'name' => 'use_normal_login_along_with_auth_method',
+                'category' => 'auth',
+                'value' => 1
+            ]);
         }
 
     }
