69a62fec6f9312ebeb7b0e5dea12221e89cc6152
オープンソース・ワークショップ 永原 篤 <nagahara@opensource-workshop.jp>
2021-01-25 10:59:07 +0900

    Laravel6対応

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -19,6 +19,8 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    /*
     protected $headers = [
         Request::HEADER_FORWARDED => 'FORWARDED',
         Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
@@ -26,4 +28,5 @@ class TrustProxies extends Middleware
         Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
         Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
     ];
+    */
 }
