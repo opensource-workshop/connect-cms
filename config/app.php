a16b752349d7c1f3096193a3b4281b9ebe5a3964
Mitsuru Mutaguchi <akagane99@gmail.com>
2021-01-25 11:33:35 +0900

    change: Laravel6で無くなったクラス Illuminate\Support\Facades\Input::class対応. Input::old() -> old()に修正 https://github.com/opensource-workshop/connect-cms/issues/714

--- config/app.php
+++ config/app.php
@@ -226,7 +226,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Input' => Illuminate\Support\Facades\Input::class,
+        // delete: Laravel6で無くなったクラス
+        //'Input' => Illuminate\Support\Facades\Input::class,
         'Carbon' => Carbon\Carbon::class,
 
         // enums
