7baa05f6c9fbbcdb456d4648bc9230b6741be7af
Mitsuru Mutaguchi <akagane99@gmail.com>
2021-01-27 11:20:47 +0900

    delete: 不要コメント削除 2021_01_18_153658_create_users_columns_table

--- database/migrations/2021_01_18_153658_create_users_columns_table.php
+++ database/migrations/2021_01_18_153658_create_users_columns_table.php
@@ -22,7 +22,6 @@ class CreateUsersColumnsTable extends Migration
             $table->string('caption_color', 255)->default('text-dark')->comment('キャプション文字色');
             $table->string('place_holder', 255)->nullable()->comment('プレースホルダ―');
 
-            // $table->integer('minutes_increments')->default(10)->comment('分刻み指定');           // 時間型の追加ルール, 一旦実装しない
             $table->string('rule_allowed_numeric', 255)->nullable()->comment('数値のみ許容');
             $table->string('rule_allowed_alpha_numeric', 255)->nullable()->comment('英数値のみ許容');
             $table->string('rule_digits_or_less', 255)->nullable()->comment('指定桁数以下を許容');
@@ -30,7 +29,6 @@ class CreateUsersColumnsTable extends Migration
             $table->string('rule_min', 255)->nullable()->comment('最小値設定');
             $table->string('rule_regex', 255)->nullable()->comment('正規表現設定');
             $table->string('rule_word_count', 255)->nullable()->comment('最大文字数（半角換算）');
-            // $table->string('rule_date_after_equal', 255)->nullable()->comment('～日以降を許容');   // 日付型の追加ルール, 一旦実装しない
 
             // 一旦実装しない
             // $table->integer('role_display_control_flag')->default(0)->comment('権限で表示カラムを制御');
