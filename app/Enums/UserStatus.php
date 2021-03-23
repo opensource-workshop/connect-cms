2fb7f68ccfb213ca8e0b71505490347e03c21273
Mitsuru Mutaguchi <akagane99@gmail.com>
2021-03-23 17:07:12 +0900

    change: ユーザ状態の承認待ち⇒仮登録に修正

--- app/Enums/UserStatus.php
+++ app/Enums/UserStatus.php
@@ -12,12 +12,12 @@ final class UserStatus extends EnumsBase
     // 定数メンバ
     const active = 0;
     const not_active = 1;
-    const approval_pending = 2;
+    const temporary = 2;
 
     // key/valueの連想配列
     const enum = [
         self::active => '利用可能',
         self::not_active => '利用不可',
-        self::approval_pending => '承認待ち',
+        self::temporary => '仮登録'
     ];
 }
