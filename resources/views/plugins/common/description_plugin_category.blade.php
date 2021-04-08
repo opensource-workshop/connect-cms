22dbf751b2953b6e9d16f4f221ccd7445a3a060f
Mitsuru Mutaguchi <akagane99@gmail.com>
2021-04-08 11:51:27 +0900

    change: プラグインのカテゴリ設定の共通説明に「文字色」「背景色」の説明追記

--- resources/views/plugins/common/description_plugin_category.blade.php
+++ resources/views/plugins/common/description_plugin_category.blade.php
@@ -7,9 +7,10 @@
 --}}
 <div class="card card-body bg-light p-2 m-2">
     <ul>
-        <li>クラス名は cc_category_xxxx で使用できます。</li>
         <li>サイト全体での共通カテゴリは<a href="{{ url('/manage/site/categories') }}" target="_blank">管理画面</a>から設定することができます。</li>
         <li>共通カテゴリは表示設定、及び、表示順のみ各プラグイン側で設定することが可能です。</li>
         <li>共通カテゴリ以外にも、本画面にてプラグイン単位の個別カテゴリを設定することが可能です。</li>
+        <li>「文字色」「背景色」にはHTMLで指定できる色キーワード（例：red, blue）やRGB色（例：#000000）等を設定できます。</li>
+        <li>クラス名は cc_category_[クラス名] で使用できます。</li>
     </ul>
 </div>
