c08dab053983af77baf4db7e75cc3d6a522adcbe
Mitsuru Mutaguchi <akagane99@gmail.com>
2021-04-13 18:18:07 +0900

    add: 課題管理のLearningtasksモデル、UserableNohistory対応

--- app/Models/User/Learningtasks/Learningtasks.php
+++ app/Models/User/Learningtasks/Learningtasks.php
@@ -3,9 +3,19 @@
 namespace App\Models\User\Learningtasks;
 
 use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\SoftDeletes;
+
+use App\UserableNohistory;
 
 class Learningtasks extends Model
 {
+    // [TODO] delated_atカラムはあるが、下記は指定されてなかった。joinなどで使ってるので、delated_at is null の修正が必要。
+    // 論理削除
+    // use SoftDeletes;
+
+    // 保存時のユーザー関連データの保持（履歴なしUserable）
+    use UserableNohistory;
+
     /**
      * レポート提出機能の使用有無
      */
@@ -28,9 +38,8 @@ class Learningtasks extends Model
         return false;
     }
 
-
     /**
-     *  レポート使用有無の文字列表記
+     * レポート使用有無の文字列表記
      */
     public function strUseReport()
     {
