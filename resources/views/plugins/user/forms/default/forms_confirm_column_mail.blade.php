0b5efa1c342f4245646a34b0b16a4bba40e2f224
Mitsuru Mutaguchi <akagane99@gmail.com>
2021-04-16 17:37:25 +0900

    bugfix: フォーム、まとめ行にチェックボックスを含めて、フォーム登録すると確認画面で500エラーになるバグ修正 https://github.com/opensource-workshop/connect-cms/issues/810

---
+++ resources/views/plugins/user/forms/default/forms_confirm_column_mail.blade.php
@@ -0,0 +1,5 @@
+{{--
+ * 確認画面(confirm mail)テンプレート。
+--}}
+{{$request->forms_columns_value[$form_obj->id]}}
+<input name="forms_columns_value[{{$form_obj->id}}]" class="form-control" type="hidden" value="{{$request->forms_columns_value[$form_obj->id]}}">
