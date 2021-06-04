96700aa734d2c13b83a4bb69a4da9f88bd2e0022
Masato Inoue <masamasamasato0216@gmail.com>
2021-06-04 14:20:58 +0900

    新着 onerowテンプレートにももっと見る機能を適用＆script部分を共通化

--- resources/views/plugins/user/whatsnews/default/whatsnews.blade.php
+++ resources/views/plugins/user/whatsnews/default/whatsnews.blade.php
@@ -92,46 +92,7 @@
 </div>
 
     @if ($whatsnews_frame->read_more_use_flag == UseType::use)
-        <script>
-            const app_{{ $frame->id }} = new Vue({
-                el: "#app_{{ $frame->id }}",
-                data: function() {
-                    return {
-                        url: '{{ url('/') }}',
-                        link_pattern: @json($link_pattern),
-                        link_base: @json($link_base),
-                        whatsnewses: [],
-                        whatsnews_total_count: {{ $whatsnews_total_count }}, // 総件数
-                        view_posted_at: {{ $whatsnews_frame->view_posted_at }},
-                        view_posted_name: {{ $whatsnews_frame->view_posted_name }},
-                        limit: {{ $whatsnews_frame->read_more_fetch_count }},
-                        offset: {{ $whatsnews->count() }} // 何件目から取得するか（＝現時点の取得件数）※初期値はサーバから返された一覧件数
-                    }
-                },
-                methods: {
-                    searchWhatsnewses: function () {
-                        let self = this;
-                        // 非同期通信で追加の一覧を取得
-                        axios.get("{{url('/')}}/json/whatsnews/indexJson/{{$page->id}}/{{$frame_id}}/?limit=" + this.limit + "&offset=" + this.offset)
-                            .then(function(res){
-                                // foreach内ではthisでvueインスタンスのwhatsnewsesが参照できない為、tmp_arrに一時的に代入
-                                tmp_arr = self.whatsnewses;
-                                res.data.forEach(function(obj) {
-                                    // 取得した差分をループしてtmp_arrに格納
-                                    tmp_arr.push(obj);
-                                });
-                                // vueインスタンスのwhatsnewsesに代入
-                                this.whatsnewses = tmp_arr;
-                            })
-                            .catch(function (error) {
-                                console.log(error)
-                            });
-                        // offset値をカウントアップ
-                        this.offset += this.limit;
-                    }
-                },
-            });
-        </script>
+        @include('plugins.user.whatsnews.whatsnews_script')
     @endif
 @endif
 @endsection
