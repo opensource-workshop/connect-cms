e57fb9b4547d902372d989a2c1f5b6a69ae75098
Mitsuru Mutaguchi <akagane99@gmail.com>
2021-06-04 12:39:27 +0900

    bugfix: 課題管理、各課題で設定できる総合評価設定を設定しても、総合評価設定がONにならないバグ修正

--- resources/views/plugins/user/learningtasks/default/learningtasks_edit_evaluate.blade.php
+++ resources/views/plugins/user/learningtasks/default/learningtasks_edit_evaluate.blade.php
@@ -65,8 +65,18 @@
     <div class="collapse {{$tool->getSettingShowstr("post_evaluate_setting")}} collapse_post_evaluate_setting" id="collapse_post_evaluate_setting">
         <h5><span class="badge badge-secondary">課題独自の項目設定</span></h5>
 
+        <div class="form-group row mb-0">
+            <label class="col-md-3 text-md-right">総合評価機能</label>
+            <div class="col-md-9 d-md-flex">
+                <div class="custom-control custom-checkbox mr-3">
+                    <input type="checkbox" name="post_settings[use_evaluate]" value="on" class="custom-control-input" id="use_evaluate" @if(old("post_settings.use_evaluate", $tool->getFunction('use_evaluate', true)) == 'on') checked=checked @endif>
+                    <label class="custom-control-label" for="use_evaluate">評価</label>
+                </div>
+            </div>
+        </div>
+
         <div class="form-group row">
-            <label class="col-md-3 text-md-right">評価</label>
+            <label class="col-md-3 text-md-right">総合評価コメント</label>
             <div class="col-md-9 d-md-flex">
                 <div class="custom-control custom-checkbox mr-3">
                     <input type="checkbox" name="post_settings[use_evaluate_file]" value="on" class="custom-control-input" id="use_evaluate_file" @if(old("post_settings.use_evaluate_file", $tool->getFunction('use_evaluate_file', true)) == 'on') checked=checked @endif>
