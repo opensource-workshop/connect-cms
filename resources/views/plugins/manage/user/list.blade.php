70211dfb43bda04158fe50de7a47bd80044a2d64
Mitsuru Mutaguchi <akagane99@gmail.com>
2021-06-10 15:37:16 +0900

    change: ユーザ管理の一覧、作成日・更新日・最終ログイン日はY/m/d H:i形式で表示に変更

--- resources/views/plugins/manage/user/list.blade.php
+++ resources/views/plugins/manage/user/list.blade.php
@@ -239,8 +239,8 @@ use App\Models\Core\UsersColumns;
                                             <option value="{{$users_column->id}}_asc" @if(Session::get('user_search_condition.sort') == $users_column->id . '_asc') selected @endif>{{  $users_column->column_name  }}(昇順)</option>
                                             <option value="{{$users_column->id}}_desc" @if(Session::get('user_search_condition.sort') == $users_column->id . '_desc') selected @endif>{{  $users_column->column_name  }}(降順)</option>
                                         @endforeach
-                                        <option value="logged_in_at_asc"@if(Session::get('user_search_condition.sort') == "logged_in_at_asc") selected @endif>最終ログイン日 昇順</option>
-                                        <option value="logged_in_at_desc"@if(Session::get('user_search_condition.sort') == "logged_in_at_desc") selected @endif>最終ログイン日 降順</option>
+                                        <option value="logged_in_at_asc"@if(Session::get('user_search_condition.sort') == "logged_in_at_asc") selected @endif>最終ログイン日時 昇順</option>
+                                        <option value="logged_in_at_desc"@if(Session::get('user_search_condition.sort') == "logged_in_at_desc") selected @endif>最終ログイン日時 降順</option>
                                     </select>
                                 </div>
                             </div>
@@ -300,9 +300,9 @@ use App\Models\Core\UsersColumns;
                     <th nowrap>権限</th>
                     <th nowrap>役割設定</th>
                     <th nowrap>状態</th>
-                    <th nowrap>作成日</th>
-                    <th nowrap>更新日</th>
-                    <th nowrap>最終ログイン日</th>
+                    <th nowrap>作成日時</th>
+                    <th nowrap>更新日時</th>
+                    <th nowrap>最終ログイン日時</th>
                 </tr>
             </thead>
             <tbody>
@@ -341,11 +341,11 @@ use App\Models\Core\UsersColumns;
                         {{$user->convertLoopValue('user_original_roles', 'value')}}
                     </td>
                     <td nowrap>{{UserStatus::getDescription($user->status)}}</td>
-                    <td>{{$user->created_at->format('Y/m/d')}}</td>
-                    <td>{{$user->updated_at->format('Y/m/d')}}</td>
-                    <td nowrap>
+                    <td>{{$user->created_at->format('Y/m/d H:i')}}</td>
+                    <td>{{$user->updated_at->format('Y/m/d H:i')}}</td>
+                    <td>
                         <a href="{{url('/')}}/manage/user/loginHistory/{{$user->id}}" title="ログイン履歴"><i class="far fa-edit"></i></a>
-                        {{$user->max_logged_in_at ? (new Carbon($user->max_logged_in_at))->format('Y/m/d') : ''}}
+                        {{$user->max_logged_in_at ? (new Carbon($user->max_logged_in_at))->format('Y/m/d H:i') : ''}}
                     </td>
                 </tr>
             @endforeach
