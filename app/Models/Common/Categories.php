6665a722ff340b6449625b33293dcef1afb43989
Mitsuru Mutaguchi <akagane99@gmail.com>
2021-06-16 17:58:42 +0900

    bugfix: カテゴリモデル、created_idが自動登録されていなかったため、登録されるよう修正

--- app/Models/Common/Categories.php
+++ app/Models/Common/Categories.php
@@ -5,15 +5,15 @@ namespace App\Models\Common;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-use App\Userable;
+use App\UserableNohistory;
 
 class Categories extends Model
 {
     // 論理削除
     use SoftDeletes;
 
-    // 保存時のユーザー関連データの保持
-    use Userable;
+    // 保存時のユーザー関連データの保持（履歴なしUserable）
+    use UserableNohistory;
 
     /**
      * create()やupdate()で入力を受け付ける ホワイトリスト
