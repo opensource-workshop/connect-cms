66ac61baf87e304edb593338e872e597817d7036
Mitsuru Mutaguchi <akagane99@gmail.com>
2021-06-17 14:36:42 +0900

    delete: AuthServiceProvider, user->roleは存在しないDBカラムのため機能してない。そのため削除

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,9 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Auth;
 
 use App\Providers\ConnectEloquentUserProvider;
-use Auth;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -27,36 +27,6 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        // システム管理者のみ許可
-        Gate::define('system', function ($user) {
-            return ($user->role == 1);
-        });
-        // サイト管理者のみ許可
-        Gate::define('site-admin', function ($user) {
-            return ($user->role == 2);
-        });
-        // ユーザ管理者のみ許可
-        Gate::define('user-admin', function ($user) {
-            return ($user->role == 3);
-        });
-        // 運用管理者のみ許可
-        Gate::define('manager', function ($user) {
-            return ($user->role == 10);
-        });
-        // 承認者のみ許可
-        Gate::define('approver', function ($user) {
-            return ($user->role == 11);
-        });
-        // 編集者のみ許可
-        Gate::define('editor', function ($user) {
-            return ($user->role == 12);
-        });
-
-        // システム管理者＆ユーザ管理者のみ許可
-        Gate::define('system_user-admin', function ($user) {
-            return ($user->role == 1 || $user->role == 3);
-        });
 
         // ユーザー認証はカスタマイズしたもので行う。
         Auth::provider('connect_eloquent', function ($app, array $config) {
