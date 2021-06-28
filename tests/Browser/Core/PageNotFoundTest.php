25ea96bb86e78f8e79d67a2706b771adddf0ee14
Mitsuru Mutaguchi <akagane99@gmail.com>
2021-06-28 18:14:00 +0900

    add: test:404, 404ページ設定後に、ページなしテスト追加

--- tests/Browser/Core/PageNotFoundTest.php
+++ tests/Browser/Core/PageNotFoundTest.php
@@ -6,6 +6,9 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
+use App\Models\Core\Configs;
+use App\Models\Common\Page;
+
 class PageNotFoundTest extends DuskTestCase
 {
     /**
@@ -21,6 +24,39 @@ class PageNotFoundTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser->visit('/not-found')
                     ->assertSee('404 Not found');
+            parent::screenshot($browser);
+        });
+    }
+
+    /**
+     * 404ページ設定後に、ページなしテスト
+     *
+     * @return void
+     *
+     * @group core
+     */
+    public function testPageNotFoundSetting404()
+    {
+        // --- 更新
+        // 404ページ登録更新
+        $page = Page::updateOrCreate(
+            ['permanent_link' => '/404'],
+            [
+                'page_name' => '404',
+                'permanent_link' => '/404',
+            ]
+        );
+
+        // 404
+        $configs = Configs::updateOrCreate(
+            ['name'     => 'page_permanent_link_404'],
+            ['category' => 'page_error',
+             'value'    => '/404']
+        );
+
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/not-found')
+                ->assertDontSee('404 Not found');   // 404ページは作ったけど、プラグインを配置していないので「404 Not found」表示がない事をチェック
             parent::screenshot($browser);
         });
     }
