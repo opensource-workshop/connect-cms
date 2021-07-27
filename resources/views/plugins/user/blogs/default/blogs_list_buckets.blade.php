addd6a8f99f478a21bcf92707a76f465e6be17ed
Mitsuru Mutaguchi <akagane99@gmail.com>
2021-07-28 08:46:23 +0900

    change: ブログ選択、ページ送り（paginate）が左右エリアに配置するとはみ出る事に対応 #940

--- resources/views/plugins/user/blogs/default/blogs_list_buckets.blade.php
+++ resources/views/plugins/user/blogs/default/blogs_list_buckets.blade.php
@@ -18,45 +18,42 @@
     {{ csrf_field() }}
     <input type="hidden" name="redirect_path" value="{{url('/')}}/plugin/blogs/listBuckets/{{$page->id}}/{{$frame_id}}#frame-{{$frame_id}}">
 
-    <div class="form-group">
-        <table class="table table-hover {{$frame->getSettingTableClass()}}">
-            <thead>
-                <tr>
-                    <th></th>
-                    <th>ブログ名</th>
-                    <th>件数</th>
-                    <th>詳細</th>
-                    <th>作成日</th>
+    <table class="table table-hover {{$frame->getSettingTableClass()}}">
+        <thead>
+            <tr>
+                <th></th>
+                <th>ブログ名</th>
+                <th>件数</th>
+                <th>詳細</th>
+                <th>作成日</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($blogs as $blog)
+                <tr @if ($blog_frame->bucket_id == $blog->bucket_id) class="cc-active-tr"@endif>
+                    <td class="d-table-cell">
+                        <input type="radio" value="{{$blog->bucket_id}}" name="select_bucket"@if ($blog_frame->bucket_id == $blog->bucket_id) checked @endif>
+                    </td>
+                    <td><span class="{{$frame->getSettingCaptionClass()}}">ブログ名：</span>{{$blog->blog_name}}</td>
+                    <td nowrap>
+                        <span class="{{$frame->getSettingCaptionClass()}}">件数：</span>{{$blog->entry_count}}
+                    </td>
+                    <td>
+                        <span class="{{$frame->getSettingCaptionClass()}}">詳細：</span>
+                        <a class="btn btn-success btn-sm" href="{{url('/')}}/plugin/blogs/editBuckets/{{$page->id}}/{{$frame_id}}/{{$blog->id}}#frame-{{$frame_id}}">
+                            <i class="far fa-edit"></i> 設定変更
+                        </a>
+                    </td>
+                    <td><span class="{{$frame->getSettingCaptionClass()}}">作成日：</span>{{$blog->created_at}}</td>
                 </tr>
-            </thead>
-            <tbody>
-                @foreach($blogs as $blog)
-                    <tr @if ($blog_frame->bucket_id == $blog->bucket_id) class="cc-active-tr"@endif>
-                        <td class="d-table-cell">
-                            <input type="radio" value="{{$blog->bucket_id}}" name="select_bucket"@if ($blog_frame->bucket_id == $blog->bucket_id) checked @endif>
-                        </td>
-                        <td><span class="{{$frame->getSettingCaptionClass()}}">ブログ名：</span>{{$blog->blog_name}}</td>
-                        <td nowrap>
-                            <span class="{{$frame->getSettingCaptionClass()}}">件数：</span>{{$blog->entry_count}}
-                        </td>
-                        <td>
-                            <span class="{{$frame->getSettingCaptionClass()}}">詳細：</span>
-                            <a class="btn btn-success btn-sm" href="{{url('/')}}/plugin/blogs/editBuckets/{{$page->id}}/{{$frame_id}}/{{$blog->id}}#frame-{{$frame_id}}">
-                                <i class="far fa-edit"></i> 設定変更
-                            </a>
-                        </td>
-                        <td><span class="{{$frame->getSettingCaptionClass()}}">作成日：</span>{{$blog->created_at}}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+            @endforeach
+        </tbody>
+    </table>
 
-    <div class="form-group text-center">
-        {{ $blogs->fragment('frame-' . $frame_id)->links() }}
-    </div>
+    {{-- ページング処理 --}}
+    @include('plugins.common.user_paginate', ['posts' => $blogs, 'frame' => $frame, 'aria_label_name' => 'ブログ選択'])
 
-    <div class="form-group text-center mt-3">
+    <div class="text-center mt-3">
         <a class="btn btn-secondary mr-2" href="{{URL::to($page->permanent_link)}}">
             <i class="fas fa-times"></i><span class="{{$frame->getSettingButtonCaptionClass('md')}}"> キャンセル</span>
         </a>
