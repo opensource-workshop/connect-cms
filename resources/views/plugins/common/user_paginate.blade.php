7eb6c4c97f123b76867092b6a654a1cb1acc2dcf
Mitsuru Mutaguchi <akagane99@gmail.com>
2021-07-27 22:23:02 +0900

    change: 固定記事選択、ページ送り（paginate）が左右エリアに配置するとはみ出る事に対応 #940

--- resources/views/plugins/common/user_paginate.blade.php
+++ resources/views/plugins/common/user_paginate.blade.php
@@ -3,16 +3,21 @@
  *
  * @param $posts ページングがあるデータ
  * @param $frame フレーム
- * @param $aria_label_name aria-label名
+ * @param $sort ソート (任意)
+ * @param $aria_label_name aria-label名 (任意)
 --}}
 @php
     $aria_label = isset($aria_label_name) ? "{$aria_label_name}のページ付け" : 'ページ付け';
-    // dd($frame);
+    $sort = isset($sort) ? $sort : null;
 @endphp
 
 {{-- アクセシビリティ対応。1ページしかない時に、空navを表示するとスクリーンリーダーに不要な Navigation がひっかかるため表示させない。 --}}
 @if ($posts->lastPage() > 1)
     <nav class="text-center" aria-label="{{$aria_label}}">
-        {{ $posts->fragment('frame-' . $frame->id)->links(null, ['frame' => $frame]) }}
+        @if (is_null($sort))
+            {{ $posts->fragment('frame-' . $frame->id)->links(null, ['frame' => $frame]) }}
+        @else
+            {{ $posts->appends(['sort' => $sort])->fragment('frame-' . $frame->id)->links(null, ['frame' => $frame]) }}
+        @endif
     </nav>
 @endif
