7eef8d2bfa628f17d414bdb431392b96bb997729
Mitsuru Mutaguchi <akagane99@gmail.com>
2021-09-02 18:23:04 +0900

    add: オプションプラグイン, オプションAPIプラグインのベースクラス追加

---
+++ app/PluginsOption/Api/ApiPluginOptionBase.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\PluginsOption\Api;
+
+use App\Plugins\Api\ApiPluginBase;
+
+/**
+ * オプション・APIプラグイン
+ *
+ * オプション・APIプラグインの基底クラス
+ *
+ * @author 牟田口 満 <mutaguchi@opensource-workshop.jp>
+ * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
+ * @category APIプラグイン
+ * @package Controller
+ */
+class ApiPluginOptionBase extends ApiPluginBase
+{
+    // 2021-09-02 現在、処理は入れていないが、今後、オプションプラグイン特有のチェックなどが入ると想定しています。
+}
