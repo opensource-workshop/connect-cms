5bc0c404a99cd2b74bf08646d38754d907db4514
Mitsuru Mutaguchi <akagane99@gmail.com>
2021-10-14 16:32:51 +0900

    change: enums, EnumsBaseの継承対応

--- app/Enums/DayOfWeek.php
+++ app/Enums/DayOfWeek.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\App;
 /**
  * 曜日
  */
-final class DayOfWeek
+final class DayOfWeek extends EnumsBase
 {
     // 定数メンバ
     const sun = 0;
@@ -20,39 +20,39 @@ final class DayOfWeek
 
     // key/valueの連想配列 日本語版
     const enum_ja = [
-        self::sun=>'日',
-        self::mon=>'月',
-        self::tue=>'火',
-        self::wed=>'水',
-        self::thu=>'木',
-        self::fri=>'金',
-        self::sat=>'土',
+        self::sun => '日',
+        self::mon => '月',
+        self::tue => '火',
+        self::wed => '水',
+        self::thu => '木',
+        self::fri => '金',
+        self::sat => '土',
     ];
 
     // key/valueの連想配列 英語版
     const enum_en = [
-        self::sun=>'sun',
-        self::mon=>'mon',
-        self::tue=>'tue',
-        self::wed=>'wed',
-        self::thu=>'thu',
-        self::fri=>'fri',
-        self::sat=>'sat',
+        self::sun => 'sun',
+        self::mon => 'mon',
+        self::tue => 'tue',
+        self::wed => 'wed',
+        self::thu => 'thu',
+        self::fri => 'fri',
+        self::sat => 'sat',
     ];
 
     /*
-    * 対応した和名を返す
-    */
+     * 対応した和名を返す
+     */
     public static function getDescription($key): string
     {
-        return App::getLocale() == \ConnectLocale::en ? self::enum_en[$key] : self::enum_ja[$key];
+        return App::getLocale() == ConnectLocale::en ? self::enum_en[$key] : self::enum_ja[$key];
     }
 
     /*
-    * key/valueの連想配列を返す
-    */
+     * key/valueの連想配列を返す
+     */
     public static function getMembers()
     {
-        return App::getLocale() == \ConnectLocale::en ? self::enum_en : self::enum_ja;
+        return App::getLocale() == ConnectLocale::en ? self::enum_en : self::enum_ja;
     }
 }
