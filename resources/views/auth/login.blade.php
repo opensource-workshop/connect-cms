9258bcfcf9ad3b95c6322ec0633bf5d30090456e
Masato Inoue <masamasamasato0216@gmail.com>
2021-10-21 14:49:51 +0900

    リファクタリング 不要コメント除去（Laravelデフォルトログイン仕様のemailコメントアウト）

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,21 +24,6 @@
                                 @endif
                             </div>
                         </div>
-{{--
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
---}}
                         <div class="form-group row{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
 
