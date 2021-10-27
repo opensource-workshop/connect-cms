1a3d50a4737b0e80c6fb7eafb51daeae23b68074
Mitsuru Mutaguchi <akagane99@gmail.com>
2021-10-27 14:14:47 +0900

    comment: コードスタディ, クラスコメントの記述間違い修正

--- app/Plugins/User/Codestudies/CodestudiesPlugin.php
+++ app/Plugins/User/Codestudies/CodestudiesPlugin.php
@@ -17,12 +17,12 @@ use App\Models\User\Codestudies\Codestudies;
 use App\Plugins\User\UserPluginBase;
 
 /**
- * ブログプラグイン
+ * コードスタディプラグイン
  *
  * @author 永原　篤 <nagahara@opensource-workshop.jp>
  * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
- * @category コンテンツプラグイン
- * @package Contoroller
+ * @category コードスタディプラグイン
+ * @package Controller
  */
 class CodestudiesPlugin extends UserPluginBase
 {
