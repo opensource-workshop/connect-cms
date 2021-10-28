66a8ab255ac6f4afd4c197b8ecaf87f0476ed13b
Mitsuru Mutaguchi <akagane99@gmail.com>
2021-10-28 15:40:51 +0900

    change: 新着, 誤字修正

--- app/Plugins/User/Whatsnews/WhatsnewsPlugin.php
+++ app/Plugins/User/Whatsnews/WhatsnewsPlugin.php
@@ -74,10 +74,9 @@ class WhatsnewsPlugin extends UserPluginBase
         // 標準権限以外で設定画面などから呼ばれる権限の定義
         // 標準権限は右記で定義 config/cc_role.php
         //
-        // 権限チェックテーブル
-        // [TODO] 【各プラグイン】declareRoleファンクションで適切な追加の権限定義を設定する https://github.com/opensource-workshop/connect-cms/issues/658
-        $role_ckeck_table = array();
-        return $role_ckeck_table;
+        // 権限チェックテーブル (追加チェックなし)
+        $role_check_table = [];
+        return $role_check_table;
     }
 
     /**
