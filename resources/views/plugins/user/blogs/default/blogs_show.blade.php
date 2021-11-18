adb388ce482c8d81aadb43d7677f9a97c3acbd11
Mitsuru Mutaguchi <akagane99@gmail.com>
2021-11-18 12:00:03 +0900

    change: ブログの詳細画面, ボタンのaタグclass修正

--- resources/views/plugins/user/blogs/default/blogs_show.blade.php
+++ resources/views/plugins/user/blogs/default/blogs_show.blade.php
@@ -142,8 +142,8 @@
             @if ($post->status == 1)
                 <span class="badge badge-warning align-bottom">一時保存</span>
             @endif
-            <a href="{{url('/')}}/plugin/blogs/edit/{{$page->id}}/{{$frame_id}}/{{$post->id}}#frame-{{$frame->id}}">
-                <span class="btn btn-success btn-sm"><i class="far fa-edit"></i> <span class="hidden-xs">編集</span></span>
+            <a href="{{url('/')}}/plugin/blogs/edit/{{$page->id}}/{{$frame_id}}/{{$post->id}}#frame-{{$frame->id}}" class="btn btn-success btn-sm">
+                <i class="far fa-edit"></i> <span class="hidden-xs">編集</span>
             </a>
         @endcan
         </div>
@@ -154,16 +154,16 @@
 <nav class="row" aria-label="{{$blog_frame->blog_name}}のページ移動">
     <div class="col-12 text-center mt-3">
         @if (isset($before_post))
-        <a href="{{url('/')}}/plugin/blogs/show/{{$page->id}}/{{$frame_id}}/{{$before_post->id}}#frame-{{$frame->id}}" class="mr-1">
-            <span class="btn btn-info"><i class="fas fa-chevron-left"></i> <span class="hidden-xs">{{__('messages.previous')}}</span></span>
+        <a href="{{url('/')}}/plugin/blogs/show/{{$page->id}}/{{$frame_id}}/{{$before_post->id}}#frame-{{$frame->id}}" class="btn btn-info">
+            <i class="fas fa-chevron-left"></i> <span class="hidden-xs">{{__('messages.previous')}}</span>
         </a>
         @endif
-        <a href="{{url('/')}}{{$page->getLinkUrl()}}#frame-{{$frame->id}}">
-            <span class="btn btn-info"><i class="fas fa-list"></i> <span class="hidden-xs">{{__('messages.to_list')}}</span></span>
+        <a href="{{url('/')}}{{$page->getLinkUrl()}}#frame-{{$frame->id}}" class="btn btn-info">
+            <i class="fas fa-list"></i> <span class="hidden-xs">{{__('messages.to_list')}}</span>
         </a>
         @if (isset($after_post))
-        <a href="{{url('/')}}/plugin/blogs/show/{{$page->id}}/{{$frame_id}}/{{$after_post->id}}#frame-{{$frame->id}}" class="mr-1">
-            <span class="btn btn-info"><i class="fas fa-chevron-right"></i> <span class="hidden-xs">{{__('messages.next')}}</span></span>
+        <a href="{{url('/')}}/plugin/blogs/show/{{$page->id}}/{{$frame_id}}/{{$after_post->id}}#frame-{{$frame->id}}" class="btn btn-info">
+            <i class="fas fa-chevron-right"></i> <span class="hidden-xs">{{__('messages.next')}}</span>
         </a>
         @endif
     </div>
