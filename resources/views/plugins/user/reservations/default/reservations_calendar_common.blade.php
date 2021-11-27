3a81df8fb6a099cb1df3ce57bb9d59d886ebfef8
Mitsuru Mutaguchi <akagane99@gmail.com>
2021-11-27 11:09:14 +0900

    change: 施設予約, 一覧の月・週タブ表示の見直し

--- resources/views/plugins/user/reservations/default/reservations_calendar_common.blade.php
+++ resources/views/plugins/user/reservations/default/reservations_calendar_common.blade.php
@@ -17,7 +17,7 @@
         <li class="nav-item">
             {{-- 月タブ --}}
             <a href="{{url('/')}}/plugin/reservations/month/{{$page->id}}/{{$frame->id}}/{{ Carbon::now()->format('Ym') }}#frame-{{$frame->id}}"
-                class="nav-link{{ $view_format == ReservationCalendarDisplayType::month ? ' active' : '' }}"
+                class="nav-link pt-0 pb-0 {{ $view_format == ReservationCalendarDisplayType::month ? ' active' : '' }}"
             >
                 {{ __('messages.month') }}
             </a>
@@ -25,7 +25,7 @@
         <li class="nav-item">
             {{-- 週タブ --}}
             <a href="{{url('/')}}/plugin/reservations/week/{{$page->id}}/{{$frame->id}}/{{ Carbon::today()->format('Ymd') }}#frame-{{$frame->id}}"
-                class="nav-link{{ $view_format == ReservationCalendarDisplayType::week ? ' active' : '' }}"
+                class="nav-link pt-0 pb-0 {{ $view_format == ReservationCalendarDisplayType::week ? ' active' : '' }}"
             >
                 {{ __('messages.week') }}
             </a>
