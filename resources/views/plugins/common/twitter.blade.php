f9ec7e90abe730e1db799b4934238572463b2ca9
オープンソース・ワークショップ 永原 篤 <nagahara@opensource-workshop.jp>
2022-01-05 18:09:34 +0900

    Update twitter.blade.php

--- resources/views/plugins/common/twitter.blade.php
+++ resources/views/plugins/common/twitter.blade.php
@@ -11,7 +11,7 @@
 --}}
 @if (FrameConfig::getConfigValueAndOld($frame_configs, BlogFrameConfig::blog_display_twitter_button) == ShowType::show)
 <a class="btn btn-sm btn-link btn-light border"
-   href="javascript:void window.open('{{urlencode("http://twitter.com/intent/tweet?text=$post_title ")}}','_blank');">
+   href="javascript:void window.open('{{urlencode("http://twitter.com/intent/tweet?text=$post_title ")}}{{urlencode(url("/"))}}','_blank');">
    <h6 class="d-inline"><i class="fab fa-twitter"></i></h6>
 </a>
 @endif
