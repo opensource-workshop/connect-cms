9f6d8793475c4a87704ae79f74b4b1bbe1653f94
オープンソース・ワークショップ 永原 篤 <nagahara@opensource-workshop.jp>
2022-01-20 17:07:14 +0900

    Update PhotoalbumsPlugin.php

--- app/Plugins/User/Photoalbums/PhotoalbumsPlugin.php
+++ app/Plugins/User/Photoalbums/PhotoalbumsPlugin.php
@@ -557,7 +557,7 @@ class PhotoalbumsPlugin extends UserPluginBase
     private function overwriteFile($file, $photoalbum_content, $page_id, $target_column = 'upload_id')
     {
         // uploads テーブルの上書き更新
-        Uploads::find($photoalbum_content->$target_column)->update([
+        Uploads::updateOrCreate(['id' => $photoalbum_content->$target_column],[
             'client_original_name' => $file->getClientOriginalName(),
             'mimetype'             => $file->getClientMimeType(),
             'extension'            => $file->getClientOriginalExtension(),
