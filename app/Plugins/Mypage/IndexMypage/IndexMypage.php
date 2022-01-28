c1435b8ea0faea229ba1e21434a97266a6a0caff
horitada <m.horiguchi.info@gmail.com>
2022-01-28 09:57:17 +0900

    add: mypage出力

--- app/Plugins/Mypage/IndexMypage/IndexMypage.php
+++ app/Plugins/Mypage/IndexMypage/IndexMypage.php
@@ -2,6 +2,9 @@
 
 namespace app\Plugins\Mypage\IndexMypage;
 
+use Illuminate\Support\Facades\Auth;
+use App\User;
+use App\Plugins\Manage\UserManage\UsersTool;
 use App\Plugins\Mypage\MypagePluginBase;
 
 /**
@@ -9,6 +12,17 @@ use App\Plugins\Mypage\MypagePluginBase;
  */
 class IndexMypage extends MypagePluginBase
 {
+    public function getPublicFunctions()
+    {
+        // 標準関数以外で画面などから呼ばれる関数の定義
+        $functions = array();
+        $functions['get']  = [
+                              'passPdfDownload',
+                              'certPdfDownload',
+                            ];
+        return $functions;
+    }
+
     /**
      *  ページ初期表示
      *
@@ -16,10 +30,17 @@ class IndexMypage extends MypagePluginBase
      */
     public function index($request)
     {
+        // ログインしているユーザー情報を取得
+        $user = Auth::user();
+        $user_input_cols = UsersTool::getUsersInputCols($user);
         // 管理画面プラグインの戻り値の返し方
         // view 関数の第一引数に画面ファイルのパス、第二引数に画面に渡したいデータを名前付き配列で渡し、その結果のHTML。
         return view('plugins.mypage.index.index', [
-            "plugin_name"  => "index",
+            "plugin_name"     => "index",
+            "function"        => __FUNCTION__,
+            "id"              => $user->id,
+            "user"            => $user,
+            "user_input_cols" => $user_input_cols,
         ]);
     }
 }
