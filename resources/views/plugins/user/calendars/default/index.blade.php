29bcc6f1095337f82f1dc3dbb36e15ab48aea6f0
Mitsuru Mutaguchi <akagane99@gmail.com>
2022-02-09 08:06:58 +0900

    change: カレンダー, posts.createをループ外で判定

--- resources/views/plugins/user/calendars/default/index.blade.php
+++ resources/views/plugins/user/calendars/default/index.blade.php
@@ -22,6 +22,13 @@
     </div>
 </div>
 
+{{-- posts.createをループ外で判定 --}}
+@can('posts.create',[[null, $frame->plugin_name, $buckets]])
+    @php $can_posts_create = true; @endphp
+@else
+    @php $can_posts_create = false; @endphp
+@endcan
+
 <table class="table table-bordered">
     <thead>
     <tr class="thead d-none d-md-table-row">
@@ -88,12 +95,12 @@
                         </div>
                     </div>
                     <div class="col-6 text-right">
-                    @can('posts.create',[[null, 'calendars', $buckets]])
+                    @if ($can_posts_create)
                         @if (isset($frame) && $frame->bucket_id)
                             {{-- 新規登録ボタン --}}
                             <a href="{{url('/')}}/plugin/calendars/edit/{{$page->id}}/{{$frame_id}}?date={{$date->format('Y-m-d')}}#frame-{{$frame_id}}"><i class="fas fa-plus"></i></a>
                         @endif
-                    @endcan
+                    @endif
                     </div>
                 </div>
                 {{-- 祝日 --}}
