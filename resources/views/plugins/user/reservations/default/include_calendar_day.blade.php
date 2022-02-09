5f0e6fe83744325563c47ab21d96d33b4bb355b4
Mitsuru Mutaguchi <akagane99@gmail.com>
2022-02-09 17:08:23 +0900

    refactor: 施設予約, 不要なカッコ削除

--- resources/views/plugins/user/reservations/default/include_calendar_day.blade.php
+++ resources/views/plugins/user/reservations/default/include_calendar_day.blade.php
@@ -9,7 +9,7 @@
 --}}
 @if ($date == Carbon::today())
     {{-- 今日 --}}
-    @if ($date->dayOfWeek == DayOfWeek::sun || ($date->hasHoliday()))
+    @if ($date->dayOfWeek == DayOfWeek::sun || $date->hasHoliday())
         <span class="fa-stack small cc-color-sunday">
             <i class="fa fa-circle fa-stack-2x"></i>
             <i class="fa fa-inverse fa-stack-1x">{{$date->day}}</i>
@@ -27,7 +27,7 @@
     @endif
 @else
     {{-- 今日以外 --}}
-    @if ($date->dayOfWeek == DayOfWeek::sun || ($date->hasHoliday()))
+    @if ($date->dayOfWeek == DayOfWeek::sun || $date->hasHoliday())
         <span class="cc-color-sunday">{{$date->day}}</span>
     @elseif ($date->dayOfWeek == DayOfWeek::sat)
         <span class="cc-color-saturday">{{$date->day}}</span>
