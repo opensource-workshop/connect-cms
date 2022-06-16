b75182d44425caa7f5c210a2252425ee1a21b291
Mitsuru Mutaguchi <akagane99@gmail.com>
2022-06-16 21:35:21 +0900

    phpcs fix

--- app/Plugins/Mypage/IndexMypage/IndexMypage.php
+++ app/Plugins/Mypage/IndexMypage/IndexMypage.php
@@ -38,7 +38,7 @@ class IndexMypage extends MypagePluginBase
     {
         // ログインしているユーザー情報を取得
         $user = Auth::user();
-        $user_input_cols = UsersInputCols::select('users_input_cols.*', 'users_columns.column_type', 'users_columns.column_name','users_columns.display_sequence', 'uploads.client_original_name')
+        $user_input_cols = UsersInputCols::select('users_input_cols.*', 'users_columns.column_type', 'users_columns.column_name', 'users_columns.display_sequence', 'uploads.client_original_name')
             ->leftJoin('users_columns', 'users_columns.id', '=', 'users_input_cols.users_columns_id')
             ->leftJoin('uploads', 'uploads.id', '=', 'users_input_cols.value')
             ->where('users_id', $user->id)
