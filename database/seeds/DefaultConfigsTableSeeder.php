7dddabcf689fc4de19d65c13c00e7637e283b643
Mitsuru Mutaguchi <akagane99@gmail.com>
2022-06-20 14:57:50 +0900

    add: ユーザ管理, seederに本登録メール件名・フォーマットの初期値を追加する

--- database/seeds/DefaultConfigsTableSeeder.php
+++ database/seeds/DefaultConfigsTableSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 use App\Models\Core\Configs;
 
@@ -17,8 +16,8 @@ class DefaultConfigsTableSeeder extends Seeder
      */
     public function run()
     {
-        if (DB::table('configs')->count() == 0) {
-            DB::table('configs')->insert(
+        if (Configs::count() == 0) {
+            Configs::insert(
                 [
                     [
                         'name'=>'base_background_color',
@@ -101,7 +100,7 @@ class DefaultConfigsTableSeeder extends Seeder
             );
         }
 
-        if (DB::table('configs')->where('name', 'base_login_password_reset')->count() == 0) {
+        if (Configs::where('name', 'base_login_password_reset')->count() == 0) {
             // パスワードリセットの使用
             $configs = Configs::create([
                 'name' => 'base_login_password_reset',
@@ -110,7 +109,7 @@ class DefaultConfigsTableSeeder extends Seeder
             ]);
         }
 
-        if (DB::table('configs')->where('name', 'use_mypage')->count() == 0) {
+        if (Configs::where('name', 'use_mypage')->count() == 0) {
             // マイページの使用
             $configs = Configs::create([
                 'name' => 'use_mypage',
@@ -119,7 +118,7 @@ class DefaultConfigsTableSeeder extends Seeder
             ]);
         }
 
-        if (DB::table('configs')->where('name', 'use_normal_login_along_with_auth_method')->count() == 0) {
+        if (Configs::where('name', 'use_normal_login_along_with_auth_method')->count() == 0) {
             // 外部認証と併せて、通常ログインも使用
             $configs = Configs::create([
                 'name' => 'use_normal_login_along_with_auth_method',
@@ -128,7 +127,7 @@ class DefaultConfigsTableSeeder extends Seeder
             ]);
         }
 
-        if (DB::table('configs')->where('name', 'base_login_redirect_previous_page')->count() == 0) {
+        if (Configs::where('name', 'base_login_redirect_previous_page')->count() == 0) {
             // ログイン時に元いたページに遷移 設定
             $configs = Configs::create([
                 'name' => 'base_login_redirect_previous_page',
@@ -137,7 +136,7 @@ class DefaultConfigsTableSeeder extends Seeder
             ]);
         }
 
-        if (DB::table('configs')->where('name', 'user_register_after_message')->count() == 0) {
+        if (Configs::where('name', 'user_register_after_message')->count() == 0) {
             // 本登録後のメッセージ
             $configs = Configs::create([
                 'name' => 'user_register_after_message',
@@ -146,7 +145,7 @@ class DefaultConfigsTableSeeder extends Seeder
             ]);
         }
 
-        if (DB::table('configs')->where('name', 'base_header_font_color_class')->count() == 0) {
+        if (Configs::where('name', 'base_header_font_color_class')->count() == 0) {
             // 画面の基本のヘッダー文字色
             $configs = Configs::create([
                 'name' => 'base_header_font_color_class',
@@ -155,7 +154,7 @@ class DefaultConfigsTableSeeder extends Seeder
             ]);
         }
 
-        if (DB::table('configs')->where('name', 'fontsizeselect')->count() == 0) {
+        if (Configs::where('name', 'fontsizeselect')->count() == 0) {
             // wysiwygで文字サイズの使用
             $configs = Configs::create([
                 'name' => 'fontsizeselect',
@@ -164,7 +163,7 @@ class DefaultConfigsTableSeeder extends Seeder
             ]);
         }
 
-        if (DB::table('configs')->where('name', 'memory_limit_for_image_resize')->count() == 0) {
+        if (Configs::where('name', 'memory_limit_for_image_resize')->count() == 0) {
             // 画像リサイズ時のPHPメモリ数
             $configs = Configs::create([
                 'name' => 'memory_limit_for_image_resize',
@@ -179,6 +178,29 @@ class DefaultConfigsTableSeeder extends Seeder
                 'name' => 'resized_image_size_initial',
                 'category' => 'wysiwyg',
                 'value' => ResizedImageSize::getDefault(),
+            ]);
+        }
+
+        if (Configs::where('name', 'user_register_mail_subject')->count() == 0) {
+            // 本登録メール件名
+            $configs = Configs::create([
+                'name' => 'user_register_mail_subject',
+                'category' => 'user_register',
+                'value' => 'ユーザ登録が完了しました - [[site_name]]'
+            ]);
+        }
+
+        if (Configs::where('name', 'user_register_mail_format')->count() == 0) {
+            // 本登録メールフォーマット
+            $configs = Configs::create([
+                'name' => 'user_register_mail_format',
+                'category' => 'user_register',
+                'value' => '--- 登録内容
+
+[[body]]
+
+
+ユーザ登録が完了しました。登録したログインID、パスワードでログインしてください。'
             ]);
         }
 
