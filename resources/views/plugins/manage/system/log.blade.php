6c28fbb42677668375281b25656b914a74160582
Mitsuru Mutaguchi <akagane99@gmail.com>
2022-06-30 23:30:01 +0900

    comment: bladeのヘッダコメント修正

--- resources/views/plugins/manage/system/log.blade.php
+++ resources/views/plugins/manage/system/log.blade.php
@@ -3,7 +3,7 @@
  *
  * @author 永原　篤 <nagahara@opensource-workshop.jp>
  * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
- * @category ログ設定
+ * @category システム管理
  --}}
 {{-- 管理画面ベース画面 --}}
 @extends('plugins.manage.manage')
