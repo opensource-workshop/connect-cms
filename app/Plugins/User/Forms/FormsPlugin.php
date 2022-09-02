670d6f862ea8a208b73a13059f8e306db6986cba
Mitsuru Mutaguchi <akagane99@gmail.com>
2022-09-02 15:33:43 +0900

    bugfix: UploadedFile::getClientSize to getSize

--- app/Plugins/User/Forms/FormsPlugin.php
+++ app/Plugins/User/Forms/FormsPlugin.php
@@ -721,7 +721,7 @@ class FormsPlugin extends UserPluginBase
                         'client_original_name' => $request->file($req_filename)->getClientOriginalName(),
                         'mimetype'             => $request->file($req_filename)->getClientMimeType(),
                         'extension'            => $request->file($req_filename)->getClientOriginalExtension(),
-                        'size'                 => $request->file($req_filename)->getClientSize(),
+                        'size'                 => $request->file($req_filename)->getSize(),
                         'plugin_name'          => 'forms',
                         'page_id'              => $page_id,
                         'temporary_flag'       => 1,
