d25abb87a8b82d8f22ff744ac5be682e5584b53a
Mitsuru Mutaguchi <akagane99@gmail.com>
2022-09-09 17:19:22 +0900

    change: OPAC, 貸し出しリクエスト画面, 入力項目エリアの微調整

--- resources/views/plugins/user/opacs/default/opacs_lent.blade.php
+++ resources/views/plugins/user/opacs/default/opacs_lent.blade.php
@@ -46,10 +46,10 @@
         @else
             <form action="{{url('/')}}/plugin/opacs/requestLent/{{$page->id}}/{{$frame_id}}/{{$opacs_books_id}}#frame-{{$frame_id}}" id="form_requestLent" name="form_requestLent" method="POST">
                 {{ csrf_field() }}
-                <div class="row">
-                
+                <div class="form-row">
+
                     {{-- 学籍番号/教職員番号　通常ユーザーは自ログインID固定 --}}
-                    <div class="col-12 col-sm-12 col-md-6 mt-2">
+                    <div class="form-group col-12 col-sm-12 col-md-6">
                         @can("role_article")
                             {{-- 権限ありの場合 --}}
                             <label class="control-label">学籍番号/教職員番号</label> <label class="badge badge-danger">必須</label>
@@ -57,45 +57,41 @@
                         @else
                             {{-- 権限なしの場合 --}}
                             <label class="control-label">学籍番号/教職員番号</label>
-                            <input type="hidden" name="req_student_no" value="{{old('req_student_no', Auth::user()->userid)}}" class="form-control">
+                            <input type="hidden" name="req_student_no" value="{{old('req_student_no', Auth::user()->userid)}}">
                             <br /><div class="card p-2" style="background-color: lightgray">{{Auth::user()->userid}}</div>
                         @endcan
 
                     </div>
 
-                    <div class="col-12 col-sm-12 col-md-6 mt-2">
+                    <div class="form-group col-12 col-sm-12 col-md-6">
                         <label class="control-label">連絡先電話番号</label> <label class="badge badge-danger">必須</label>
                         <input type="text" name="req_phone_no" value="{{old('req_phone_no')}}" class="form-control" placeholder="ハイフンなしで半角数値">
                         @if ($errors && $errors->has('req_phone_no')) <div class="text-danger">{{$errors->first('req_phone_no')}}</div> @endif
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-12 mt-2">
-                        <label class="control-label">連絡先メールアドレス</label> <label class="badge badge-danger">必須</label>
-                        <input type="text" name="req_email" value="{{old('req_email')}}" class="form-control">
-                        @if ($errors && $errors->has('req_email')) <div class="text-danger">{{$errors->first('req_email')}}</div> @endif
-                    </div>
+                <div class="form-group">
+                    <label class="control-label">連絡先メールアドレス</label> <label class="badge badge-danger">必須</label>
+                    <input type="text" name="req_email" value="{{old('req_email')}}" class="form-control">
+                    @if ($errors && $errors->has('req_email')) <div class="text-danger">{{$errors->first('req_email')}}</div> @endif
                 </div>
-                    <div class="row">
-                    <div class="col-12 col-md-4 mt-2">
+                <div class="form-row">
+                    <div class="form-group col-12 col-md-4">
                         <label class="control-label">郵送先郵便番号</label> <label class="badge badge-danger">必須</label>
                         <input type="text" name="req_postal_code" value="{{old('req_postal_code')}}" class="form-control" placeholder="ハイフンなしで半角数値">
                         @if ($errors && $errors->has('req_postal_code')) <div class="text-danger">{{$errors->first('req_postal_code')}}</div> @endif
                     </div>
-                    <div class="col-12 col-md-8 mt-2">
+                    <div class="form-group col-12 col-md-8">
                         <label class="control-label">郵送先住所</label> <label class="badge badge-danger">必須</label>
                         <input type="text" name="req_address" value="{{old('req_address')}}" class="form-control">
                         @if ($errors && $errors->has('req_address')) <div class="text-danger">{{$errors->first('req_address')}}</div> @endif
                     </div>
                 </div>
-                <div class="row mb-3">
-                    <div class="col-12 mt-2">
-                        <label class="control-label">郵送先宛名</label>
-                        <input type="text" name="req_mailing_name" value="{{old('req_mailing_name')}}" class="form-control" placeholder="郵送先の宛名がユーザー名と異なる場合は入力してください。">
-                        @if ($errors && $errors->has('req_mailing_name')) <div class="text-danger">{{$errors->first('req_mailing_name')}}</div> @endif
-                    </div>
+                <div class="form-group">
+                    <label class="control-label">郵送先宛名</label>
+                    <input type="text" name="req_mailing_name" value="{{old('req_mailing_name')}}" class="form-control" placeholder="郵送先の宛名がユーザー名と異なる場合は入力してください。">
+                    @if ($errors && $errors->has('req_mailing_name')) <div class="text-danger">{{$errors->first('req_mailing_name')}}</div> @endif
                 </div>
-                <div class="row mb-3">
+                <div class="form-group form-row">
                     <div class="col-12 text-center">
                         <button type="button" class="btn btn-success mr-3" onclick="location.href='{{url('/')}}{{$page->getLinkUrl()}}'"><i class="fas fa-list"></i> 一覧へ戻る</button>
                         <button type="button" class="btn btn-primary" onclick="javascript:form_requestLent.submit();"><i class="fas fa-check"></i> リクエストする</button>
