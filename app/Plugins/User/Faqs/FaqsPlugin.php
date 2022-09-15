cc534deb6f14264b218c280794496be9ba6e5a08
gakigaki <e3e3e3d@gmail.com>
2022-09-15 19:27:21 +0900

    Fix: 並び順取得元
    plugin_categoriesにプラグインごとのカテゴリ設定が入っている

--- app/Plugins/User/Faqs/FaqsPlugin.php
+++ app/Plugins/User/Faqs/FaqsPlugin.php
@@ -230,7 +230,7 @@ class FaqsPlugin extends UserPluginBase
                 'categories.background_color as category_background_color',
                 'categories.category as category',
                 'plugin_categories.view_flag as category_view_flag',
-                'categories.display_sequence as category_display_sequence',
+                'plugin_categories.display_sequence as category_display_sequence',
             )
             ->whereIn('faqs_posts.id', function ($query) use ($faq_frame) {
                 $query->select(DB::raw('MAX(id) As id'))
