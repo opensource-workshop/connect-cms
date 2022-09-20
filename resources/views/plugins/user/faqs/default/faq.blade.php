0cffae70911b754125cd85a967dedd3452a999d6
gakigaki <e3e3e3d@gmail.com>
2022-09-20 11:45:22 +0900

    Improve: author追加

--- resources/views/plugins/user/faqs/default/faq.blade.php
+++ resources/views/plugins/user/faqs/default/faq.blade.php
@@ -2,6 +2,7 @@
  * FAQの1レコード部
  *
  * @author 永原　篤 <nagahara@opensource-workshop.jp>
+ * @author 石垣 佑樹 <ishigaki@opensource-workshop.jp>
  * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
  * @category FAQプラグイン
  * @param boolean $hide_category カテゴリを隠すか
