39bf12be14293a383517378897c2774e5129bf1c
オープンソース・ワークショップ 永原 篤 <nagahara@opensource-workshop.jp>
2022-09-26 16:18:36 +0900

    Revert "ソース整理"
    This reverts commit 7d9002c9adac001183e78d4fd156901b44b471e7.

---
+++ app/Models/User/Receives/ReceiveRecord.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\User\Receives;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ReceiveRecord extends Model
+{
+    //
+    protected $fillable = ['receive_id'];
+}
