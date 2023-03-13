91fe12f1d78d548635255b7b432095eb044d64ee
Mitsuru Mutaguchi <akagane99@gmail.com>
2023-03-13 14:43:40 +0900

    add: empty_bucket_setting.bladeのplugin_nameを指定できるオプションを追加

--- resources/views/plugins/common/empty_bucket_setting.blade.php
+++ resources/views/plugins/common/empty_bucket_setting.blade.php
@@ -4,6 +4,8 @@
  * @author 牟田口 満 <mutaguchi@opensource-workshop.jp>
  * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
  * @category プラグイン共通
+ *
+ * @param $plugin_name   プラグイン名
 --}}
 @extends('core.cms_frame_base_setting')
 
@@ -17,9 +19,13 @@
 
 @section("plugin_setting_$frame->id")
 
+@php
+$plugin_name = $plugin_name ?? $frame->plugin_name_full;
+@endphp
+
 {{-- バケツなし --}}
 <div class="alert alert-warning">
-    <i class="fas fa-exclamation-circle"></i> {{ __('messages.empty_bucket_setting', ['plugin_name' => $frame->plugin_name_full]) }}
+    <i class="fas fa-exclamation-circle"></i> {{ __('messages.empty_bucket_setting', ['plugin_name' => $plugin_name]) }}
 </div>
 
 @endsection
