121aea34c957b0ed43bc838612b829a2714a9aee
makino <34463938+osws-makino@users.noreply.github.com>
2023-05-25 15:25:59 +0900

    bugfix:サイト内検索デフォルトテンプレートバグ修正

--- resources/views/plugins/user/searchs/default/searchs_result.blade.php
+++ resources/views/plugins/user/searchs/default/searchs_result.blade.php
@@ -1,7 +1,7 @@
 {{--
  * 検索画面
  *
- * @author 永原　篤 <nagahara@opensource-workshop.jp>
+ * @author 永原　篤 <nagahara@opensource-workshop.jp> / 牧野　可也子 <makino@opensource-workshop.jp>
  * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
  * @category 検索プラグイン
 --}}
@@ -17,40 +17,50 @@
     </div>
 @endif
 
-@if ($searchs_results)
-<div class="mt-3">
-    <dl>
+@if($searchs_results)
+<div class="searchs-result-body mt-3">
     @foreach($searchs_results as $searchs_result)
-        @if ($searchs_frame->view_posted_at)
+    <dl class="searchs-result-row border-bottom">
         <dt>
-            @if ($searchs_frame->view_posted_at)
+            {{-- 登録日 --}}
+            @if($searchs_frame->view_posted_at)
                 {{(new Carbon($searchs_result->posted_at))->format('Y/m/d')}}
-                @if($searchs_result->category)
-                    <span class="badge cc_category_{{$searchs_result->classname}}">{{$searchs_result->category}}</span>
-                @endif
+            @endif
+            {{-- カテゴリ --}}
+            @if($searchs_result->category)
+                <span class="badge cc_category_{{$searchs_result->classname}}">{{$searchs_result->category}}</span>
             @endif
         </dt>
-        @endif
-        <dd>
-            @if ($link_pattern[$searchs_result->plugin_name] == 'show_page_frame_post')
+        <dd class="searchs-result-posttitle">
+            {{-- タイトル --}}
+            @if($link_pattern[$searchs_result->plugin_name] == 'show_page_frame_post')
             <a href="{{url('/')}}{{$link_base[$searchs_result->plugin_name]}}/{{$searchs_result->page_id}}/{{$searchs_result->frame_id}}/{{$searchs_result->post_id}}#frame-{{$searchs_result->frame_id}}">
-                {{$searchs_result->post_title}}
-            </a>
-            @elseif ($link_pattern[$searchs_result->plugin_name] == 'show_page')
+            @elseif($link_pattern[$searchs_result->plugin_name] == 'show_page')
             <a href="{{url('/')}}{{$searchs_result->permanent_link}}">
-                {{$searchs_result->post_title}}
-            </a>
+            @else
+            {{-- 上記以外は想定していない為、取り敢えず permanent_link をリンク先とする --}}
+            <a href="{{url('/')}}{{$searchs_result->permanent_link}}">
             @endif
-            @if ($searchs_frame->view_posted_name)
+            @if(!empty(strip_tags($searchs_result->post_title)))
+                {{-- タイトル  本文と同じぐらいで取り敢えずカット。 --}}
+                {!! mb_strimwidth(strip_tags($searchs_result->post_title), 0, 160, '…') !!}
+            @else
+                (無題)
+            @endif
+            </a>
+            {{-- 登録者 --}}
+            @if($searchs_frame->view_posted_name)
                 - {{$searchs_result->posted_name}}
             @endif
         </dd>
         {{-- 本文 半角160文字（全角80文字）まで 世の検索エンジンがだいたいこれくらい --}}
-        <dd class="text-secondary search-result-body">
+        @if(!empty(strip_tags($searchs_result->body)))
+        <dd class="search-result-postbody text-secondary">
             {!! mb_strimwidth(strip_tags($searchs_result->body), 0, 160, '…') !!}
         </dd>
-    @endforeach
+        @endif
     </dl>
+    @endforeach
 @php
     $appends['search_keyword'] = old('search_keyword');
     // ページ配下の絞込み
