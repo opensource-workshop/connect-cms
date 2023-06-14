9bc276cfd1aebaa652c08cf7eddc74f07703a341
Mitsuru Mutaguchi <akagane99@gmail.com>
2023-06-14 23:38:14 +0900

    delete: [移行ツール] NC3移行エクスポート, /cabinets/cabinet_files/download/ のリンク置換は、インポート側で既に行っていたため、現在動いてないプログラムを削除

--- app/Traits/Migration/MigrationNc3ExportTrait.php
+++ app/Traits/Migration/MigrationNc3ExportTrait.php
@@ -6155,10 +6155,7 @@ trait MigrationNc3ExportTrait
 
         // 画像の中の wysiwygのdownload をエクスポートしたパスに変換する。
         $content = $this->nc3MigrationCommonDownloadMain($nc3_frame, $save_folder, $ini_filename, $content, $img_srcs, '[upload_images]');
-        // [TODO] 未対応
-        // cabinet_action_main_download をエクスポート形式に変換
-        // [upload_images]に追記したいので、nc2MigrationCommonDownloadMainの直後に実行
-        // $content = $this->nc3MigrationCabinetActionMainDownload($save_folder, $ini_filename, $content, 'src');
+        // /cabinets/cabinet_files/download/ をエクスポート形式に変換は不要。MigrationTrait::convertNc3PluginPermalink() プラグイン固有リンク置換 で同じ処理されてるため。
 
         // CSS の img-fluid を自動で付ける最小の画像幅
         $img_fluid_min_width = $this->getMigrationConfig('wysiwyg', 'img_fluid_min_width', 0);
@@ -6175,10 +6172,7 @@ trait MigrationNc3ExportTrait
         $anchors = MigrationUtils::getContentAnchor($content);
         // 添付ファイルの中の wysiwygのdownload をエクスポートしたパスに変換する。
         $content = $this->nc3MigrationCommonDownloadMain($nc3_frame, $save_folder, $ini_filename, $content, $anchors, '[upload_files]');
-        // [TODO] 未対応
-        // cabinet_action_main_download をエクスポート形式に変換
-        // [upload_files]に追記したいので、nc2MigrationCommonDownloadMainの直後に実行
-        // $content = $this->nc3MigrationCabinetActionMainDownload($save_folder, $ini_filename, $content, 'href');
+        // /cabinets/cabinet_files/download/ をエクスポート形式に変換は不要。MigrationTrait::convertNc3PluginPermalink() プラグイン固有リンク置換 で同じ処理されてるため。
 
         // Google Analytics タグ部分を削除
         $content = MigrationUtils::deleteGATag($content);
@@ -6271,44 +6265,6 @@ trait MigrationNc3ExportTrait
         // パスを変更した記事を返す。
         return array($content, $export_paths);
     }
-
-    /**
-     * NC3：cabinet_action_main_download をエクスポート形式に変換
-     */
-    private function nc3MigrationCabinetActionMainDownload($save_folder, $ini_filename, $content, $attr = 'href')
-    {
-        //?action=cabinet_action_main_download&block_id=778&room_id=1&cabinet_id=9&file_id=2020&upload_id=5688
-        $pattern = '/'. $attr.'=".*?\?action=cabinet_action_main_download&.*?upload_id=([0-9]+)"/i';
-        if (preg_match_all($pattern, $content, $cabinet_downloads)) {
-            $cabinet_file_ids = $cabinet_downloads[1];
-            $replace_key_vals = [];
-            foreach ($cabinet_file_ids as $key => $file_id) {
-                // 移行したアップロードファイルをini ファイルから探す
-                if ($this->uploads_ini && array_key_exists('uploads', $this->uploads_ini) && array_key_exists($file_id, $this->uploads_ini['uploads']['upload'])) {
-                    $path = '../../uploads/' . $this->uploads_ini[$file_id]['temp_file_name'];
-                    $replace_href_pattern = '/'. $attr.'="(.*?\?action=cabinet_action_main_download&.*?upload_id='. $file_id.')"/i';
-                    if (preg_match_all($replace_href_pattern, $content, $m)) {
-                        $match_href = $m[1][0];
-                        $replace_key_vals[$key] = [ 'upload_id' => $file_id,
-                                                    'match_href' => $match_href,
-                                                    'path' => $path,
-                        ];
-                    }
-                }
-            }
-            // 既にnc2MigrationCommonDownloadMainでiniファイルに追記されているので、[upload_files]は空にする
-            $ini_text = '';
-            foreach ($replace_key_vals as $vals) {
-                $content = str_replace($vals['match_href'], $vals['path'], $content);
-                $ini_text .= 'upload[' . $vals['upload_id'] . "] = \"" . $vals['path'] . "\"\n";
-            }
-            if ($ini_filename) {
-                $this->storageAppend($save_folder . "/" . $ini_filename, $ini_text);
-            }
-        }
-        return $content;
-    }
-
 
     /**
      * NC3の記事ステータスからConnectのステータスへ変換
