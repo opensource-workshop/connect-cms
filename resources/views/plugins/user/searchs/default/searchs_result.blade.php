09b7ef8e531f36383fed9fdb1ae8309a56b421d5
makino <34463938+osws-makino@users.noreply.github.com>
2023-06-21 16:52:46 +0900

    bugfix:サイト内検索デフォルトテンプレートバグ修正
    バグではなく機能改善となる部分を削りました。

--- resources/views/plugins/user/searchs/default/searchs_result.blade.php
+++ resources/views/plugins/user/searchs/default/searchs_result.blade.php
@@ -18,9 +18,9 @@
 @endif
 
 @if($searchs_results)
-<div class="searchs-result-body mt-3">
+<div class="mt-3">
+    <dl>
     @foreach($searchs_results as $searchs_result)
-    <dl class="searchs-result-row border-bottom">
         <dt>
             {{-- 登録日 --}}
             @if($searchs_frame->view_posted_at)
@@ -31,7 +31,7 @@
                 <span class="badge cc_category_{{$searchs_result->classname}}">{{$searchs_result->category}}</span>
             @endif
         </dt>
-        <dd class="searchs-result-posttitle">
+        <dd>
             {{-- タイトル --}}
             @if($link_pattern[$searchs_result->plugin_name] == 'show_page_frame_post')
             <a href="{{url('/')}}{{$link_base[$searchs_result->plugin_name]}}/{{$searchs_result->page_id}}/{{$searchs_result->frame_id}}/{{$searchs_result->post_id}}#frame-{{$searchs_result->frame_id}}">
@@ -54,13 +54,11 @@
             @endif
         </dd>
         {{-- 本文 半角160文字（全角80文字）まで 世の検索エンジンがだいたいこれくらい --}}
-        @if(!empty(strip_tags($searchs_result->body)))
-        <dd class="search-result-postbody text-secondary">
+        <dd class="text-secondary search-result-body border-bottom">
             {!! mb_strimwidth(strip_tags($searchs_result->body), 0, 160, '…') !!}
         </dd>
-        @endif
-    </dl>
     @endforeach
+    </dl>
 @php
     $appends['search_keyword'] = old('search_keyword');
     // ページ配下の絞込み
