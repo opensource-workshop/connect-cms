39e12ad5d6f1f687c984b7d4371efc20d349f2e3
masayuki horiguchi <m.horiguchi.info@gmail.com>
2023-07-14 11:51:58 +0900

    Update migrate_main.blade.php
    NC3移行データ出力PDFの修正

--- resources/views/plugins/manage/site/pdf/migrate_main.blade.php
+++ resources/views/plugins/manage/site/pdf/migrate_main.blade.php
@@ -12,8 +12,8 @@
 <h2 style="text-align: center; font-size: 28px;">移行データ</h2>
 <br />
 <h4>移行データ一覧</h4>
-@if ($nc2_sort_pages)
-    <h5>NC2移行対象ページ一覧</h5>
+@if ($mg_sort_pages)
+    <h5>移行対象ページ一覧</h5>
     <table border="0" class="table_css">
         <tr nobr="true">
             <th class="doc_th" style="width: 5%;">No.</th>
@@ -23,17 +23,17 @@
         @php 
             $cnt=0;
         @endphp
-        @foreach($nc2_sort_pages as $nc2_page)
+        @foreach($mg_sort_pages as $mg_page)
             @php 
             $cnt++;
             @endphp
             <tr nobr="true">
-                <td>{{$cnt}}</td><td>{{$nc2_page->page_name}}</td><td>{{$nc2_page->permalink}}</td>
+                <td>{{$cnt}}</td><td>{{$mg_page->page_name}}</td><td>{{$mg_page->permalink}}</td>
             </tr>
         @endforeach
     </table>
     <div>
-    NC2移行対象ページ件数：{{$cnt}}
+    移行対象ページ件数：{{$cnt}}
     <div style="font-size: 10px;">※migration_configで除外定義したデータを含みます</div>
     </div>
 @endif
@@ -63,8 +63,8 @@
     </div>
 @endif
 
-@if ($nc2_journals)
-    <h5>NC2移行対象ブログ</h5>
+@if ($mg_blogs)
+    <h5>移行対象ブログ</h5>
     <table border="0" class="table_css">
         <tr nobr="true">
             <th class="doc_th" style="width: 5%;">No.</th>
@@ -74,12 +74,12 @@
         @php 
             $cnt=0;
         @endphp
-        @foreach($nc2_journals as $nc2_journal)
+        @foreach($mg_blogs as $mg_blog)
             @php 
             $cnt++;
             @endphp
             <tr nobr="true">
-                <td>{{$cnt}}</td><td>{{$nc2_journal->journal_name}}</td><td>{{$nc2_journal->count}}</td>
+                <td>{{$cnt}}</td><td>{{$mg_blog->blog_name}}</td><td>{{$mg_blog->count}}</td>
             </tr>
         @endforeach
     </table>
@@ -108,8 +108,8 @@
     <div style="font-size: 10px;">※{{date('Y年m月d日')}}時点でのデータになります</div>
 @endif
 
-@if ($nc2_multidatabases)
-    <h5>NC2移行対象汎用DB</h5>
+@if ($mg_multidatabases)
+    <h5>移行対象汎用DB</h5>
     <table border="0" class="table_css">
         <tr nobr="true">
             <th class="doc_th" style="width: 5%;">No.</th>
@@ -119,12 +119,12 @@
         @php 
             $cnt=0;
         @endphp
-        @foreach($nc2_multidatabases as $nc2_multidatabase)
+        @foreach($mg_multidatabases as $mg_multidatabase)
             @php 
             $cnt++;
             @endphp
             <tr nobr="true">
-                <td>{{$cnt}}</td><td>{{$nc2_multidatabase->multidatabase_name}}</td><td>{{$nc2_multidatabase->multidatabase_content_count}}</td>
+                <td>{{$cnt}}</td><td>{{$mg_multidatabase->multidatabase_name}}</td><td>{{$mg_multidatabase->multidatabase_content_count}}</td>
             </tr>
         @endforeach
     </table>
@@ -153,8 +153,8 @@
     <div style="font-size: 10px;">※{{date('Y年m月d日')}}時点でのデータになります</div>
 @endif
 
-@if ($nc2_users)
-    <h5>NC2移行対象会員</h5>
+@if ($mg_users)
+    <h5>移行対象会員</h5>
     <table border="0" class="table_css">
         <tr nobr="true">
             <th class="doc_th" style="width: 5%;">No.</th>
@@ -165,12 +165,12 @@
         @php 
             $cnt=0;
         @endphp
-        @foreach($nc2_users as $nc2_user)
+        @foreach($mg_users as $mg_user)
             @php 
             $cnt++;
             @endphp
             <tr nobr="true">
-                <td>{{$cnt}}</td><td>{{$nc2_user->login_id}}</td><td>{{$nc2_user->handle}}</td><td>{{$nc2_user->role_authority_name}}</td>
+                <td>{{$cnt}}</td><td>{{$mg_user->login_id}}</td><td>{{$mg_user->handle}}</td><td>{{$mg_user->role_authority_name}}</td>
             </tr>
         @endforeach
     </table>
