d647a7df79b564539abbf45bf86ec66a748dc9c7
オープンソース・ワークショップ 永原 篤 <nagahara@opensource-workshop.jp>
2023-08-22 15:29:05 +0900

    WYSIWYGのテンプレート編集機能の追加です。
    管理機能のテーマ管理にて、WYSIWYGのテンプレートを編集できる機能を追加しました。

---
+++ resources/views/plugins/manage/theme/theme_template_edit.blade.php
@@ -0,0 +1,35 @@
+{{--
+ * テンプレート編集テンプレート
+ *
+ * @author 永原　篤 <nagahara@opensource-workshop.jp>
+ * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
+ * @category テーマ管理
+ --}}
+{{-- 管理画面ベース画面 --}}
+@extends('plugins.manage.manage')
+
+{{-- 管理画面メイン部分のコンテンツ section:manage_content で作ること --}}
+@section('manage_content')
+
+<div class="card mb-3">
+    <div class="card-header p-0">
+        {{-- 機能選択タブ --}}
+        @include('plugins.manage.theme.theme_manage_tab')
+    </div>
+    <div class="card-body">
+
+        <form action="{{url('/')}}/manage/theme/saveTemplate" method="POST">
+            {{csrf_field()}}
+            <input name="dir_name" type="hidden" value="{{$dir_name}}" />
+            <textarea name="template" class="form-control" rows=20 placeholder="（例）&#13;.navbar-dark .navbar-brand {&#13;    color: #ffff00; # デフォルトのヘッダーバー文字色を黄色にします。 &#13;}">{{$template}}</textarea>
+            <div class="form-group mt-3">
+                <button type="button" class="btn btn-secondary mr-2" onclick="location.href='{{url('/')}}/manage/theme/'"><i class="fas fa-times"></i> キャンセル</button>
+                <button type="submit" class="btn btn-primary form-horizontal">
+                    <i class="fas fa-check"></i> テンプレートファイル保存
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
+
+@endsection
