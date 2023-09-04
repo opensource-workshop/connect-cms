398c7e1b3d49328c32db7143a3061b5a6128db9e
Mitsuru Mutaguchi <akagane99@gmail.com>
2023-09-04 13:42:27 +0900

    add: コア, TrustProxiesを.envで設定できるように対応

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,7 +3,6 @@
 namespace App\Http\Middleware;
 
 use Fideloper\Proxy\TrustProxies as Middleware;
-use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
 {
@@ -19,14 +18,5 @@ class TrustProxies extends Middleware
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
-    /*
-    protected $headers = [
-        Request::HEADER_FORWARDED => 'FORWARDED',
-        Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
-        Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
-        Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
-        Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
-    ];
-    */
+    protected $headers;
 }
