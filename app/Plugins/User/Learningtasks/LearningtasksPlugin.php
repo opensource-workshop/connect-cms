2ed97ec37d0a5be71641828765ca059cf95e131f
Mitsuru Mutaguchi <akagane99@gmail.com>
2023-12-21 16:50:14 +0900

    refactor: CSV, レスポンス版のCSVデータ整形処理をメソッド化

--- app/Plugins/User/Learningtasks/LearningtasksPlugin.php
+++ app/Plugins/User/Learningtasks/LearningtasksPlugin.php
@@ -2233,26 +2233,7 @@ class LearningtasksPlugin extends UserPluginBase
         ];
 
         // データ
-        $csv_data = '';
-        foreach ($csv_array as $csv_line) {
-            foreach ($csv_line as $csv_col) {
-                $csv_data .= '"' . $csv_col . '",';
-            }
-            // 末尾カンマを削除
-            $csv_data = substr($csv_data, 0, -1);
-            $csv_data .= "\n";
-        }
-
-        // Log::debug(var_export($request->character_code, true));
-
-        // 文字コード変換
-        if ($request->character_code == CsvCharacterCode::utf_8) {
-            $csv_data = mb_convert_encoding($csv_data, CsvCharacterCode::utf_8);
-            // UTF-8のBOMコードを追加する(UTF-8 BOM付きにするとExcelで文字化けしない)
-            $csv_data = CsvUtils::addUtf8Bom($csv_data);
-        } else {
-            $csv_data = mb_convert_encoding($csv_data, CsvCharacterCode::sjis_win);
-        }
+        $csv_data = CsvUtils::getResponseCsvData($csv_array, $request->character_code);
 
         return response()->make($csv_data, 200, $headers);
     }
@@ -2825,7 +2806,7 @@ class LearningtasksPlugin extends UserPluginBase
     }
 
     /**
-     * CSVインポートのフォーマットダウンロード
+     * CSVインポートの試験日時フォーマットダウンロード
      */
     public function downloadCsvFormatExaminations($request, $page_id, $frame_id, $post_id)
     {
@@ -2835,7 +2816,7 @@ class LearningtasksPlugin extends UserPluginBase
     }
 
     /**
-     * データベースデータダウンロード
+     * 試験日時ダウンロード
      */
     public function downloadCsvExaminations($request, $page_id, $frame_id, $post_id, $data_output_flag = true)
     {
@@ -2885,26 +2866,7 @@ class LearningtasksPlugin extends UserPluginBase
         ];
 
         // データ
-        $csv_data = '';
-        foreach ($csv_array as $csv_line) {
-            foreach ($csv_line as $csv_col) {
-                $csv_data .= '"' . $csv_col . '",';
-            }
-            // 末尾カンマを削除
-            $csv_data = substr($csv_data, 0, -1);
-            $csv_data .= "\n";
-        }
-
-        // Log::debug(var_export($request->character_code, true));
-
-        // 文字コード変換
-        if ($request->character_code == CsvCharacterCode::utf_8) {
-            $csv_data = mb_convert_encoding($csv_data, CsvCharacterCode::utf_8);
-            // UTF-8のBOMコードを追加する(UTF-8 BOM付きにするとExcelで文字化けしない)
-            $csv_data = CsvUtils::addUtf8Bom($csv_data);
-        } else {
-            $csv_data = mb_convert_encoding($csv_data, CsvCharacterCode::sjis_win);
-        }
+        $csv_data = CsvUtils::getResponseCsvData($csv_array, $request->character_code);
 
         return response()->make($csv_data, 200, $headers);
     }
