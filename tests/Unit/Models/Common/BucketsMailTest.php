fd575c482bfe3806ea6e2184737ce81edc4aa81f
Mitsuru Mutaguchi <akagane99@gmail.com>
2024-10-04 12:15:18 +0900

    test: BucketsMailTestのsetUp()見直し

--- tests/Unit/Models/Common/BucketsMailTest.php
+++ tests/Unit/Models/Common/BucketsMailTest.php
@@ -2,14 +2,26 @@
 
 namespace Tests\Unit\Models\Common;
 
-use PHPUnit\Framework\TestCase;
-
-use App\Models\Common\BucketsMail;
-
 use App\Enums\NoticeEmbeddedTag;
+use App\Models\Common\BucketsMail;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class BucketsMailTest extends TestCase
 {
+    use RefreshDatabase;
+
+    /**
+     * 初期設定
+     */
+    protected function setUp(): void
+    {
+        $this->refreshApplication();
+        // $this->refreshDatabase();
+
+        parent::setUp();
+    }
+
     /**
      * getFormattedSubject()テスト
      *
