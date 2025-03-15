148169e5724488d8a7db12c89490e8359dd20130
Mitsuru Mutaguchi <akagane99@gmail.com>
2025-03-15 13:35:18 +0900

    change: test>FaqsPluginTest, 画面表示がおいつかない場合があるので、ちょっと待つ

--- tests/Browser/User/FaqsPluginTest.php
+++ tests/Browser/User/FaqsPluginTest.php
@@ -85,6 +85,9 @@ class FaqsPluginTest extends DuskTestCase
         $this->logout();
 
         $this->browse(function (Browser $browser) {
+            // 画面表示がおいつかない場合があるので、ちょっと待つ
+            $browser->pause(500);
+
             $browser->visit('/test/faq')
                     ->click('#categories_id_' . $this->test_frame->id)
                     ->pause(500)
