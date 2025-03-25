2f223b4cd77e737f39b3614689d3784d2e85e4c7
Mitsuru Mutaguchi <akagane99@gmail.com>
2025-03-25 12:34:36 +0900

    change: 祝日登録, 入力エラー表示は共通部品を使うように修正

--- resources/views/plugins/manage/holiday/edit.blade.php
+++ resources/views/plugins/manage/holiday/edit.blade.php
@@ -2,6 +2,7 @@
  * 祝日管理の編集画面
  *
  * @author 永原　篤 <nagahara@opensource-workshop.jp>
+ * @author 牟田口 満 <mutaguchi@opensource-workshop.jp>
  * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
  * @category 祝日管理
  --}}
@@ -18,6 +19,9 @@
     </div>
 
     <div class="card-body">
+        {{-- 共通エラーメッセージ 呼び出し --}}
+        @include('plugins.common.errors_form_line')
+
         <form name="form_edit" method="post" action="{{url('/')}}/manage/holiday/update">
             {{ csrf_field() }}
             <input type="hidden" name="post_id" value="{{$post->id}}">
@@ -33,7 +37,7 @@
                             <div class="input-group-text"><i class="far fa-clock"></i></div>
                         </div>
                     </div>
-                    @if ($errors && $errors->has('holiday_date')) <div class="text-danger">{{$errors->first('holiday_date')}}</div> @endif
+                    @include('plugins.common.errors_inline', ['name' => 'holiday_date'])
                     <script type="text/javascript">
                     $(function () {
                         $('#holiday_date').datetimepicker({
@@ -53,7 +57,7 @@
                     <div class="input-group date" id="holiday_name">
                         <input type="text" name="holiday_name" value="{{old('holiday_name', $post->holiday_name)}}" class="form-control" />
                     </div>
-                    @if ($errors && $errors->has('holiday_name')) <div class="text-danger">{{$errors->first('holiday_name')}}</div> @endif
+                    @include('plugins.common.errors_inline', ['name' => 'holiday_name'])
                 </div>
             </div>
 
