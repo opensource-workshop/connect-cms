26f0ad400cf6ebd04354b68277390cacb7525432
Mitsuru Mutaguchi <akagane99@gmail.com>
2025-03-25 16:09:24 +0900

    change: DateTimePicker, tempusdominus-bootstrap-4 → @eonasdan/tempus-dominus v6に切替

---
+++ resources/views/plugins/common/datetimepicker.blade.php
@@ -0,0 +1,72 @@
+{{--
+ * 日付入力テンプレート
+ *
+ * @author 牟田口 満 <mutaguchi@opensource-workshop.jp>
+ * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
+ * @category プラグイン共通
+--}}
+@php
+    $element_id = $element_id ?? null;
+    $side_by_side = $side_by_side ?? false;
+    $format = $format ?? 'yyyy-MM-dd HH:mm';
+    $seconds = $seconds ?? false;
+    $clock = $clock ?? true;
+    $calendar_icon = $calendar_icon ?? true;
+    $stepping = $stepping ?? 1;
+    $view_mode = $view_mode ?? 'calendar';
+@endphp
+
+<script type="text/javascript">
+    const picker_{{$element_id}} = new tempusDominus.TempusDominus(document.getElementById('{{$element_id}}'), {
+        localization: {
+            locale: '{{ App::getLocale() }}',
+            dayViewHeaderFormat: { year: 'numeric', month: 'long' },
+            format: '{{$format}}',
+            @if (App::getLocale() == ConnectLocale::ja)
+                today: '本日',
+                close: '閉じる',
+                selectMonth: '月を選択',
+                previousMonth: '前月',
+                nextMonth: '次月',
+                selectYear: '年を選択',
+                previousYear: '前年',
+                nextYear: '次年',
+                selectDecade: '期間を選択',
+                previousDecade: '前期間',
+                nextDecade: '次期間',
+                previousCentury: '前世紀',
+                nextCentury: '次世紀',
+                pickHour: '時間を取得',
+                incrementHour: '時間を増加',
+                decrementHour: '時間を減少',
+                pickMinute: '分を取得',
+                incrementMinute: '分を増加',
+                decrementMinute: '分を減少',
+                pickSecond: '秒を取得',
+                incrementSecond: '秒を増加',
+                decrementSecond: '秒を減少',
+                toggleMeridiem: '午前/午後切替',
+                selectTime: '時間を選択',
+            @endif
+        },
+        display: {
+            viewMode: '{{$view_mode}}',
+            components: {
+                @if ($seconds)
+                    seconds: true,
+                @endif
+                @if (!$clock)
+                    clock: false,
+                @endif
+                @if (!$calendar_icon)
+                    calendar: false,
+                @endif
+            },
+            @if ($side_by_side)
+                sideBySide: true,
+            @endif
+            theme: 'light',
+        },
+        stepping: {{$stepping}},
+    });
+</script>
