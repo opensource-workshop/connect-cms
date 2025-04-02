27acf68a4bfcd0a8e796720f45036799c2bc6e43
Mitsuru Mutaguchi <akagane99@gmail.com>
2025-04-02 19:41:52 +0900

    comment, 新着情報, phpdocの@author修正

--- resources/views/plugins/user/whatsnews/onerow/whatsnews.blade.php
+++ resources/views/plugins/user/whatsnews/onerow/whatsnews.blade.php
@@ -1,7 +1,9 @@
 {{--
  * 新着情報表示画面（１行表示）
  *
- * @author 永原　篤 <nagahara@opensource-workshop.jp>, 井上 雅人 <inoue@opensource-workshop.jp / masamasamasato0216@gmail.com>
+ * @author 永原　篤 <nagahara@opensource-workshop.jp>
+ * @author 井上 雅人 <inoue@opensource-workshop.jp / masamasamasato0216@gmail.com>
+ * @author 牟田口 満 <mutaguchi@opensource-workshop.jp>
  * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
  * @category 新着情報プラグイン
 --}}
