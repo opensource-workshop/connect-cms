51ac7a2cf31082ab6fa21f94bf137a4aa5341ae2
Mitsuru Mutaguchi <akagane99@gmail.com>
2025-05-05 06:42:32 +0900

    change: laravel10, GitHubのソース差分対応 laravel/laravel/compare/9.x...10.x

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -48,17 +48,17 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
 
     /**
-     * The application's route middleware.
+     * The application's middleware aliases.
      *
-     * These middleware may be assigned to groups or used individually.
+     * Aliases may be used instead of class names to conveniently assign middleware to routes and groups.
      *
-     * @var array
+     * @var array<string, class-string|string>
      */
     protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
