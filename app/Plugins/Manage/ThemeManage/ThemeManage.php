8adfc55095c7cc9d3f0bdf91249c9dcb3e65d8ed
Masato Inoue <masamasamasato0216@gmail.com>
2025-05-22 12:24:17 +0900

    change: 管理 テンプレート登録機能を復活

--- app/Plugins/Manage/ThemeManage/ThemeManage.php
+++ app/Plugins/Manage/ThemeManage/ThemeManage.php
@@ -32,7 +32,7 @@ class ThemeManage extends ManagePluginBase
         $role_ckeck_table["create"]      = array('admin_site');
         $role_ckeck_table["editCss"]     = array('admin_site');
         $role_ckeck_table["saveCss"]     = array('admin_site');
-        // $role_ckeck_table["editTemplate"] = array('admin_site');
+        $role_ckeck_table["editTemplate"] = array('admin_site');
         $role_ckeck_table["saveTemplate"] = array('admin_site');
         $role_ckeck_table["editJs"]      = array('admin_site');
         $role_ckeck_table["saveJs"]      = array('admin_site');
@@ -386,7 +386,6 @@ class ThemeManage extends ManagePluginBase
         ]);
     }
 
-    // delete: tinymce7対応. template はTinyMCE 7.xのオープンソース版から削除されてPremium版に移りました
     /**
      * テンプレート編集画面
      *
@@ -394,7 +393,6 @@ class ThemeManage extends ManagePluginBase
      * method_desc ユーザ・テーマ毎のテンプレートを画面で編集できます。
      * method_detail 保存したテンプレートは選択したテーマで反映されます。
      */
-    /*
     public function editTemplate($request, $id)
     {
         // httpメソッド確認
@@ -428,7 +426,6 @@ class ThemeManage extends ManagePluginBase
             "template"         => $template,
         ]);
     }
-    */
 
     /**
      * テンプレート保存画面
