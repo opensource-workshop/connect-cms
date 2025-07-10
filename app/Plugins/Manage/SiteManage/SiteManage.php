2d4d804711ae649056fa12d53c9d9d6349929db8
masaton0216 <inoue@opensource-workshop.co.jp>
2025-07-10 11:16:33 +0900

    refactor: プラグイン共通 サイト管理 重複チェック処理を更に共通化

--- app/Plugins/Manage/SiteManage/SiteManage.php
+++ app/Plugins/Manage/SiteManage/SiteManage.php
@@ -490,51 +490,10 @@ class SiteManage extends ManagePluginBase
     {
         /* エラーチェック
         ------------------------------------ */
-        $rules = [];
-
-        // エラーチェックの項目名
-        $setAttributeNames = [];
-
-        // 追加項目のどれかに値が入っていたら、行の他の項目も必須
-        if (!empty($request->add_display_sequence) || !empty($request->add_classname)  || !empty($request->add_category) || !empty($request->add_color) || !empty($request->add_background_color)) {
-            // 項目のエラーチェック
-            $rules['add_display_sequence'] = ['required'];
-            $rules['add_classname'] = ['required', new CustomValiUniqueClassname()];
-            $rules['add_category'] = ['required'];
-            $rules['add_color'] = ['required'];
-            $rules['add_background_color'] = ['required'];
-
-            $setAttributeNames['add_display_sequence'] = '追加行の表示順';
-            $setAttributeNames['add_classname'] = '追加行のクラス名';
-            $setAttributeNames['add_category'] = '追加行のカテゴリ';
-            $setAttributeNames['add_color'] = '追加行の文字色';
-            $setAttributeNames['add_background_color'] = '追加行の背景色';
-        }
-
-        // 既存項目のidに値が入っていたら、行の他の項目も必須
-        if (!empty($request->categories_id)) {
-            foreach ($request->categories_id as $category_id) {
-                // 項目のエラーチェック
-                $rules['display_sequence.'.$category_id] = ['required'];
-                $rules['classname.'.$category_id] = ['required', new CustomValiUniqueClassname($category_id)];
-                $rules['category.'.$category_id] = ['required'];
-                $rules['color.'.$category_id] = ['required'];
-                $rules['background_color.'.$category_id] = ['required'];
-
-                $setAttributeNames['display_sequence.'.$category_id] = '表示順';
-                $setAttributeNames['classname.'.$category_id] = 'クラス名';
-                $setAttributeNames['category.'.$category_id] = 'カテゴリ';
-                $setAttributeNames['color.'.$category_id] = '文字色';
-                $setAttributeNames['background_color.'.$category_id] = '背景色';
-            }
-        }
-
-        // 項目のエラーチェック
-        $validator = Validator::make($request->all(), $rules);
-        $validator->setAttributeNames($setAttributeNames);
-
+        // サイト管理用カテゴリバリデーションを使用
+        $validator = Categories::validateSiteCategories($request);
+        
         if ($validator->fails()) {
-            // return $this->categories($request, $id, $validator->errors());
             return redirect()->back()->withErrors($validator)->withInput();
         }
 
