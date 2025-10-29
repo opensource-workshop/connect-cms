a4d2c9cbff8405c60ce4300a236034f3e0600c35
masaton0216 <inoue@opensource-workshop.co.jp>
2025-10-29 14:48:31 +0900

    refactor: db:seed configs 未使用変数を除去

--- database/seeders/DefaultConfigsTableSeeder.php
+++ database/seeders/DefaultConfigsTableSeeder.php
@@ -164,7 +164,7 @@ class DefaultConfigsTableSeeder extends Seeder
 
         if (Configs::where('name', 'base_login_password_reset')->count() == 0) {
             // パスワードリセットの使用
-            $configs = Configs::create([
+            Configs::create([
                 'name' => 'base_login_password_reset',
                 'category' => 'general',
                 'value' => 1
@@ -173,7 +173,7 @@ class DefaultConfigsTableSeeder extends Seeder
 
         if (Configs::where('name', 'use_mypage')->count() == 0) {
             // マイページの使用
-            $configs = Configs::create([
+            Configs::create([
                 'name' => 'use_mypage',
                 'category' => 'general',
                 'value' => 1
@@ -182,7 +182,7 @@ class DefaultConfigsTableSeeder extends Seeder
 
         if (Configs::where('name', 'use_normal_login_along_with_auth_method')->count() == 0) {
             // 外部認証と併せて、通常ログインも使用
-            $configs = Configs::create([
+            Configs::create([
                 'name' => 'use_normal_login_along_with_auth_method',
                 'category' => 'auth',
                 'value' => 1
@@ -191,7 +191,7 @@ class DefaultConfigsTableSeeder extends Seeder
 
         if (Configs::where('name', 'base_login_redirect_previous_page')->count() == 0) {
             // ログイン時に元いたページに遷移 設定
-            $configs = Configs::create([
+            Configs::create([
                 'name' => 'base_login_redirect_previous_page',
                 'category' => 'general',
                 'value' => 0
@@ -200,7 +200,7 @@ class DefaultConfigsTableSeeder extends Seeder
 
         if (Configs::where('name', 'user_register_after_message')->count() == 0) {
             // 本登録後のメッセージ
-            $configs = Configs::create([
+            Configs::create([
                 'name' => 'user_register_after_message',
                 'category' => 'user_register',
                 'value' => 'ユーザ登録が完了しました。登録したログインID、パスワードでログインしてください。',
@@ -210,7 +210,7 @@ class DefaultConfigsTableSeeder extends Seeder
 
         if (Configs::where('name', 'base_header_font_color_class')->count() == 0) {
             // 画面の基本のヘッダー文字色
-            $configs = Configs::create([
+            Configs::create([
                 'name' => 'base_header_font_color_class',
                 'category' => 'general',
                 'value' => BaseHeaderFontColorClass::navbar_dark
@@ -219,7 +219,7 @@ class DefaultConfigsTableSeeder extends Seeder
 
         if (Configs::where('name', 'fontsizeselect')->count() == 0) {
             // wysiwygで文字サイズの使用
-            $configs = Configs::create([
+            Configs::create([
                 'name' => 'fontsizeselect',
                 'category' => 'wysiwyg',
                 'value' => 0
@@ -228,7 +228,7 @@ class DefaultConfigsTableSeeder extends Seeder
 
         if (Configs::where('name', 'memory_limit_for_image_resize')->count() == 0) {
             // 画像リサイズ時のPHPメモリ数
-            $configs = Configs::create([
+            Configs::create([
                 'name' => 'memory_limit_for_image_resize',
                 'category' => 'server',
                 'value' => '256M'
@@ -237,7 +237,7 @@ class DefaultConfigsTableSeeder extends Seeder
 
         if (Configs::where('name', 'resized_image_size_initial')->count() == 0) {
             // 初期に選択させる画像サイズ
-            $configs = Configs::create([
+            Configs::create([
                 'name' => 'resized_image_size_initial',
                 'category' => 'wysiwyg',
                 'value' => ResizedImageSize::getDefault(),
@@ -246,7 +246,7 @@ class DefaultConfigsTableSeeder extends Seeder
 
         if (Configs::where('name', 'user_register_mail_subject')->count() == 0) {
             // 本登録メール件名
-            $configs = Configs::create([
+            Configs::create([
                 'name' => 'user_register_mail_subject',
                 'category' => 'user_register',
                 'value' => 'ユーザ登録が完了しました - [[site_name]]',
@@ -256,7 +256,7 @@ class DefaultConfigsTableSeeder extends Seeder
 
         if (Configs::where('name', 'user_register_mail_format')->count() == 0) {
             // 本登録メールフォーマット
-            $configs = Configs::create([
+            Configs::create([
                 'name' => 'user_register_mail_format',
                 'category' => 'user_register',
                 'value' => '--- 登録内容
