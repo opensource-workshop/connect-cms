036bae419afed1b4597fb9d3b209131e2be13706
オープンソース・ワークショップ 永原 篤 <nagahara@opensource-workshop.jp>
2025-11-28 07:33:04 +0900

    処理効率化及び不要な部分を削除しました。

--- app/Http/Middleware/MultiLanguage.php
+++ app/Http/Middleware/MultiLanguage.php
@@ -24,11 +24,11 @@ class MultiLanguage
         // alternate 対応。出力例は以下の通り。
         // 多言語設定の管理範囲のURLの場合、他の言語の情報を示す。
 
-        // <link rel="alternate" href="xxx/ja" hreflang="ja" />
-        // <link rel="alternate" href="xxx/en" hreflang="en" />
+        // <link rel="alternate" href="https://example.jp/ja" hreflang="ja" />
+        // <link rel="alternate" href="https://example.jp/xxx/en" hreflang="en" />
         // or
-        // <link rel="alternate" href="xxx/ja-JP" hreflang="ja-JP" />
-        // <link rel="alternate" href="xxx/en-GB" hreflang="en-GB" />
+        // <link rel="alternate" href="https://example.jp/xxx/ja-JP" hreflang="ja-JP" />
+        // <link rel="alternate" href="https://example.jp/xxx/en-GB" hreflang="en-GB" />
 
         // Configs
         $configs = $request->attributes->get('configs');
@@ -44,16 +44,6 @@ class MultiLanguage
             // 次のmiddkewareへ。
             return $next($request);
         }
-
-        /*
-        SELECT *
-        FROM pages as p
-        INNER JOIN configs as c ON c.category = 'language' AND p.permanent_link = c.additional1
-
-        SELECT *
-        FROM pages as p
-        INNER JOIN configs as c ON c.category = 'language' AND p.permanent_link LIKE CONCAT(c.additional1, '/test')
-        */
 
         // alternate 対応。ここでは、URL パスの locale パス以降のパスを取得したい。
         // /es-ES      ⇒         (空白)
@@ -82,39 +72,24 @@ class MultiLanguage
             /en-GB       ⇒ {config->locale}/      ⇒ 言語ルートの場合、cofig を / で検索
             /en-GB/test  ⇒ {config->locale}/test
         */
+
         // カレントURLが多言語対象と判断するため、カレントURLの2つ目の / 以降を取り除いたものでconfig の多言語設定をチェック。
         // 上記のカレントURLから言語ルートを特定する例とpageの検索例のようにするために言語ルートを特定
         $language_root = $this->getLanguageRootPath($page['permanent_link']);
 
-        // 言語ルートのデバッグ表示
-        // \Log::debug($language_root);
-        // \Log::debug(strpos($page['permanent_link'], '/', 1));
-
         // 言語ルートをキーにして多言語設定を絞り込み（現在のページが多言語設定の対象か判断するため）
-        $configs = $request->attributes->get('configs');
         $configs_multilanguage = $configs->where('category', 'language')->where('additional1', $language_root);
-        // \Log::debug(print_r($configs_multilanguage, true));
 
         // 現在のページが多言語設定にない場合は次のmiddlewareへ
         if ($configs_multilanguage->isEmpty()) {
             return $next($request);
         }
 
-        // パスの2つ目の / 以前を取り除く（言語ルート以外のURLを特定する）
-        //$two_slash_pos = strpos($page['permanent_link'], '/', 1);
-        //if ($two_slash_pos === false) {
-        //    // $no_lang_path = substr($page['permanent_link'], 0, 1);
-        //    $no_lang_path = "";
-        //} else {
-        //    $no_lang_path = substr($page['permanent_link'], $two_slash_pos);
-        //}
-
         // 言語ルート以外のパスを特定する。後で他言語の言語ルートをくっつけて、同じ意味で別言語のページを検索するため。
         $no_lang_path = str_replace($language_root, '', $page['permanent_link']);
 
         // 設定されている各言語のルートパスをconfigから絞り込む
         $configs_multilanguage_roots = $configs->where('category', 'language');
-        // \Log::debug(print_r($configs_multilanguage_roots, true));
 
         // 各言語のルートパスと現在のパスを組み合わせて、whereInで現ページと同じ意味を持つ多言語ページのpermanent_linkを用意する。
         $multilanguage_ins = [];
@@ -123,13 +98,11 @@ class MultiLanguage
             // '//'の変換は、configの多言語設定URLでトレイリングスラッシュのアリ/ナシをどちらも許容しているため。
             $multilanguage_ins[str_replace('/', '', $configs_multilanguage_root->additional1)] = str_replace('//', '/', $multilanguage_in);
         }
-        // \Log::debug(print_r($multilanguage_ins, true));
 
         // Pageテーブルから現ページと同じ意味を持つ多言語ページのpermanent_linkを取得する。
-        $multilanguage_pages = Page::select('pages.*')
+        $multilanguage_pages = Page::select('permanent_link')
             ->whereIn('pages.permanent_link', $multilanguage_ins)
             ->get();
-        // \Log::debug($multilanguage_pages);
 
         // 多言語のページ一覧を画面に渡すためにリクエストに設定する。
         $alternates = [];
@@ -137,8 +110,8 @@ class MultiLanguage
             $alternates[$this->getLocale2Path($page->permanent_link)] = $page->permanent_link;
         }
         $request->attributes->add(['alternates' => $alternates]);
-        // \Log::debug(print_r($alternates, true));
 
+        // 次のミドルウェアへ
         return $next($request);
     }
 
