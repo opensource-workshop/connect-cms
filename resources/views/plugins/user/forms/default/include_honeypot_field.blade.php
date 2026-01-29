6afb1236071bbc7f7fa05597ae8f70a3fa1dcad6
masaton0216 <inoue@opensource-workshop.co.jp>
2026-01-29 16:05:32 +0900

    refactor フォーム ハニーポット用のCSSをconnect.cssに移動

--- resources/views/plugins/user/forms/default/include_honeypot_field.blade.php
+++ resources/views/plugins/user/forms/default/include_honeypot_field.blade.php
@@ -4,20 +4,9 @@
  * @author 井上 雅人 <inoue@opensource-workshop.jp / masamasamasato0216@gmail.com>
  * @copyright OpenSource-WorkShop Co.,Ltd. All Rights Reserved
  * @category フォームプラグイン
+ * @note CSSは public/css/connect.css の .connect-hp-field を参照
 --}}
 @if ($has_honeypot)
-{{-- ボットが「画面上に存在する入力欄」と誤認しやすいよう、サイズを0にして透明にする --}}
-<style>
-.connect-hp-field {
-    opacity: 0;
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 0;
-    width: 0;
-    z-index: -1;
-}
-</style>
 <div class="connect-hp-field" aria-hidden="true">
     <label for="website_url">ウェブサイトURL（入力不要項目）</label>
     <input type="text" name="website_url" id="website_url" value="" autocomplete="off" tabindex="-1">
