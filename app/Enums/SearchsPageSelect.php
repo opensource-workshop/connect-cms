a917ba1f2dc090b0ab8d0ec5f7c90200bd28c062
Mitsuru Mutaguchi <akagane99@gmail.com>
2026-04-09 09:49:34 +0900

    phpcs fix

--- app/Enums/SearchsPageSelect.php
+++ app/Enums/SearchsPageSelect.php
@@ -6,7 +6,7 @@ use App\Enums\EnumsBase;
 
 /**
  * 通知の埋め込みタグ
- * 
+ *
  * @author 牟田口 満 <akagane99@gmail.com>
  * @category サイト内検索
  * @package Enums
@@ -19,7 +19,7 @@ class SearchsPageSelect extends EnumsBase
     /** ページ管理のメニュー表示条件に従う */
     const MENU_VISIBLE_ONLY = 1;
 
-    // key/valueの連想配列
+    /** key/valueの連想配列 */
     const enum = [
         self::ALL_PAGES => '全て表示する',
         self::MENU_VISIBLE_ONLY => 'ページ管理のメニュー表示条件に従う',
